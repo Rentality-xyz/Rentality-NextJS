54d9f6942ba2b73065082a9062561419c537d715
Artem Islamov <artem.islamov@eigenmethod.com>
2023-04-27 15:22:52 +0100

    Add ABIs and Utils

---
+++ src/utils/numericFormatters.ts
@@ -0,0 +1,6 @@
+export const currencyFormat = (value: number | bigint) => {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    }).format(value)
+};
