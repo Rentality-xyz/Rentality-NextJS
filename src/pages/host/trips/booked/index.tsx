ab5fa08c1542f2bd6729fcd8fe1eb29d76bbaf0e
Artem Islamov <artem.islamov@eigenmethod.com>
2023-05-11 09:41:51 +0100

    trips (booked UI + 30% of logic and history UI )

--- src/pages/host/trips/booked/index.tsx
+++ src/pages/host/trips/booked/index.tsx
@@ -1,18 +1,33 @@
 import HostLayout from "@/components/host/layout/hostLayout";
-import TripItem from "@/components/host/tripItem";
+import TripItem, { TripInfo, TripStatus } from "@/components/host/tripItem";
+import useTripsBooked from "@/hooks/useTripsBooked";
 
 export default function Booked() {
+  const [dataFetched, tripsBooked] = useTripsBooked();
+
   return (
     <HostLayout>
-      <div className="flex flex-col pl-8 pt-4">
+      <div className="flex flex-col px-8 pt-4">
         <div className="text-2xl">Booked</div>
-        <div className="flex flex-col">
-          <TripItem />
-          <TripItem />
-          <TripItem />
-          <TripItem />
-          <TripItem />
-        </div>
+        {!dataFetched ? (
+          <div className="flex mt-5 justify-between flex-wrap max-w-screen-xl text-center">
+            Loading...
+          </div>
+        ) : (
+          <div className="flex flex-col gap-4 pr-4 my-4">
+            {tripsBooked != null && tripsBooked.length > 0 ? (
+              tripsBooked.map((value) => {
+                return (
+                  <TripItem key={value.tripId} tripInfo={value}></TripItem>
+                );
+              })
+            ) : (
+              <div className="flex mt-5 justify-between flex-wrap max-w-screen-xl text-center">
+                You dont have booked trips
+              </div>
+            )}
+          </div>
+        )}
       </div>
     </HostLayout>
   );
--- src/pages/host/trips/history/index.tsx
+++ src/pages/host/trips/history/index.tsx
@@ -1,17 +1,32 @@
 import HostLayout from "@/components/host/layout/hostLayout";
-import TripItem from "@/components/host/tripItem";
+import TripItem, { TripInfo, TripStatus } from "@/components/host/tripItem";
 
 export default function History() {
+  const mockTripInfo: TripInfo = {
+    tripId: 1,
+    carId: 1,
+    brand: "Ford",
+    model: "Mustang",
+    year: "2023",
+    licensePlate: "EE 099 TVQ",
+    image: "",
+    tripStart: "April 10, 4:00 AM",
+    tripEnd: "April 20, 4:00 AM",
+    locationStart: "Miami, CA, USA",
+    locationEnd: "Miami, CA, USA",
+    status: TripStatus.Closed,
+  };
+
   return (
     <HostLayout>
-      <div className="flex flex-col pl-8 pt-4">
+      <div className="flex flex-col px-8 pt-4">
         <div className="text-2xl">History</div>
-        <div className="flex flex-col">
-          <TripItem />
-          <TripItem />
-          <TripItem />
-          <TripItem />
-          <TripItem />
+        <div className="flex flex-col gap-4 pr-4 my-4">
+          <TripItem tripInfo={{ ...mockTripInfo, tripId: 1 }} />
+          <TripItem tripInfo={{ ...mockTripInfo, tripId: 2 }} />
+          <TripItem tripInfo={{ ...mockTripInfo, tripId: 3 }} />
+          <TripItem tripInfo={{ ...mockTripInfo, tripId: 4 }} />
+          <TripItem tripInfo={{ ...mockTripInfo, tripId: 5 }} />
         </div>
       </div>
     </HostLayout>
