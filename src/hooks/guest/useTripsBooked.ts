433b5ad2bac2754f7d7914ae33841b1662a6d6d4
Artem Islamov <artem.islamov@eigenmethod.com>
2023-05-11 11:13:54 +0100

    fix Promise.all with empty array

--- src/hooks/guest/useTripsBooked.ts
+++ src/hooks/guest/useTripsBooked.ts
@@ -2,7 +2,10 @@ import { Contract, BrowserProvider } from "ethers";
 import { useEffect, useState } from "react";
 import RentCarJSON from "../../abis";
 import { TripInfo, TripStatus } from "@/components/guest/tripItem";
-import { ContractCarToRent, validateContractCarToRent } from "@/model/blockchain/ContractCarToRent";
+import {
+  ContractCarToRent,
+  validateContractCarToRent,
+} from "@/model/blockchain/ContractCarToRent";
 
 const useTripsBooked = () => {
   const [dataFetched, setDataFetched] = useState<Boolean>(false);
@@ -33,47 +36,51 @@ const useTripsBooked = () => {
       const tripsBookedView: ContractCarToRent[] =
         await rentalityContract.getMyCars();
 
-      const tripsBookedData = await Promise.all(
-        tripsBookedView.map(async (i: ContractCarToRent, index) => {
-          if (index === 0) {
-            validateContractCarToRent(i);
-          }
-          const tokenURI = await rentalityContract.tokenURI(i.tokenId);
-          const response = await fetch(tokenURI, {
-            headers: {
-              Accept: "application/json",
-            },
-          });
-          const meta = await response.json();
+      const tripsBookedData =
+        tripsBookedView.length === 0
+          ? []
+          : await Promise.all(
+              tripsBookedView.map(async (i: ContractCarToRent, index) => {
+                if (index === 0) {
+                  validateContractCarToRent(i);
+                }
+                const tokenURI = await rentalityContract.tokenURI(i.tokenId);
+                const response = await fetch(tokenURI, {
+                  headers: {
+                    Accept: "application/json",
+                  },
+                });
+                const meta = await response.json();
 
-          const price = Number(i.pricePerDayInUsdCents) / 100;
+                const price = Number(i.pricePerDayInUsdCents) / 100;
 
-          let item: TripInfo = {
-            tripId: Number(i.tokenId),
-            carId: Number(i.tokenId),
-            image: meta.image,
-            brand:
-              meta.attributes?.find((x: any) => x.trait_type === "Brand")
-                ?.value ?? "",
-            model:
-              meta.attributes?.find((x: any) => x.trait_type === "Model")
-                ?.value ?? "",
-            year:
-              meta.attributes?.find((x: any) => x.trait_type === "Release year")
-                ?.value ?? "",
-            licensePlate:
-              meta.attributes?.find(
-                (x: any) => x.trait_type === "License plate"
-              )?.value ?? "",
-            tripStart: "April 10, 4:00 AM",
-            tripEnd: "April 10, 4:00 AM",
-            locationStart: "Miami, CA, USA",
-            locationEnd: "Miami, CA, USA",
-            status: TripStatus.Pending,
-          };
-          return item;
-        })
-      );
+                let item: TripInfo = {
+                  tripId: Number(i.tokenId),
+                  carId: Number(i.tokenId),
+                  image: meta.image,
+                  brand:
+                    meta.attributes?.find((x: any) => x.trait_type === "Brand")
+                      ?.value ?? "",
+                  model:
+                    meta.attributes?.find((x: any) => x.trait_type === "Model")
+                      ?.value ?? "",
+                  year:
+                    meta.attributes?.find(
+                      (x: any) => x.trait_type === "Release year"
+                    )?.value ?? "",
+                  licensePlate:
+                    meta.attributes?.find(
+                      (x: any) => x.trait_type === "License plate"
+                    )?.value ?? "",
+                  tripStart: "April 10, 4:00 AM",
+                  tripEnd: "April 10, 4:00 AM",
+                  locationStart: "Miami, CA, USA",
+                  locationEnd: "Miami, CA, USA",
+                  status: TripStatus.Pending,
+                };
+                return item;
+              })
+            );
 
       return tripsBookedData;
     } catch (e) {
--- src/hooks/host/useMyListings.ts
+++ src/hooks/host/useMyListings.ts
@@ -1,7 +1,10 @@
 import { Contract, BrowserProvider } from "ethers";
 import { useEffect, useState } from "react";
 import RentCarJSON from "../../abis";
-import { ContractCarToRent, validateContractCarToRent } from "@/model/blockchain/ContractCarToRent";
+import {
+  ContractCarToRent,
+  validateContractCarToRent,
+} from "@/model/blockchain/ContractCarToRent";
 import { CarInfo } from "@/model/CarInfo";
 
 const useMyListings = () => {
@@ -33,43 +36,47 @@ const useMyListings = () => {
       const myListingsView: ContractCarToRent[] =
         await rentalityContract.getMyCars();
 
-      const myListingsData = await Promise.all(
-        myListingsView.map(async (i: ContractCarToRent, index) => {
-          if (index === 0) {
-            validateContractCarToRent(i);
-          }
-          const tokenURI = await rentalityContract.tokenURI(i.tokenId);
-          const response = await fetch(tokenURI, {
-            headers: {
-              'Accept': 'application/json',
-            },
-          });
-          const meta = await response.json();
+      const myListingsData =
+        myListingsView.length === 0
+          ? []
+          : await Promise.all(
+              myListingsView.map(async (i: ContractCarToRent, index) => {
+                if (index === 0) {
+                  validateContractCarToRent(i);
+                }
+                const tokenURI = await rentalityContract.tokenURI(i.tokenId);
+                const response = await fetch(tokenURI, {
+                  headers: {
+                    Accept: "application/json",
+                  },
+                });
+                const meta = await response.json();
 
-          const price = Number(i.pricePerDayInUsdCents) / 100;
+                const price = Number(i.pricePerDayInUsdCents) / 100;
 
-          let item: CarInfo = {
-            tokenId: Number(i.tokenId),
-            owner: i.owner.toString(),
-            image: meta.image,
-            brand:
-              meta.attributes?.find((x: any) => x.trait_type === "Brand")
-                ?.value ?? "",
-            model:
-              meta.attributes?.find((x: any) => x.trait_type === "Model")
-                ?.value ?? "",
-            year:
-              meta.attributes?.find((x: any) => x.trait_type === "Release year")
-                ?.value ?? "",
-            licensePlate:
-              meta.attributes?.find(
-                (x: any) => x.trait_type === "License plate"
-              )?.value ?? "",
-            pricePerDay: price,
-          };
-          return item;
-        })
-      );
+                let item: CarInfo = {
+                  tokenId: Number(i.tokenId),
+                  owner: i.owner.toString(),
+                  image: meta.image,
+                  brand:
+                    meta.attributes?.find((x: any) => x.trait_type === "Brand")
+                      ?.value ?? "",
+                  model:
+                    meta.attributes?.find((x: any) => x.trait_type === "Model")
+                      ?.value ?? "",
+                  year:
+                    meta.attributes?.find(
+                      (x: any) => x.trait_type === "Release year"
+                    )?.value ?? "",
+                  licensePlate:
+                    meta.attributes?.find(
+                      (x: any) => x.trait_type === "License plate"
+                    )?.value ?? "",
+                  pricePerDay: price,
+                };
+                return item;
+              })
+            );
 
       return myListingsData;
     } catch (e) {
