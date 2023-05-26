46d54c1ab09fe8b4f28979fc8cc8bdd9ead625e5
Artem Islamov <artem.islamov@eigenmethod.com>
2023-05-26 11:42:28 +0100

    add abis for sepolia test network

--- src/abis/index.js
+++ src/abis/index.js
@@ -1,8 +1,8 @@
 import RentCarJSONNet from "./RentCar.json";
 import RentCarJSONLocal from "./RentCar.localhost.json";
-import RentalityJSONNet from "./Rentality.localhost.json";
+import RentalityJSONNet from "./Rentality.json";
 import RentalityJSONLocal from "./Rentality.localhost.json";
-import RentalityCurrencyConverterJSONNet from "./RentalityCurrencyConverter.localhost.json";
+import RentalityCurrencyConverterJSONNet from "./RentalityCurrencyConverter.json";
 import RentalityCurrencyConverterJSONLocal from "./RentalityCurrencyConverter.localhost.json";
 
 const rentCarJSON = (process.env.NEXT_PUBLIC_USE_LOCALHOST_BLOCKCHAIN)?.toLowerCase?.() === "true"
