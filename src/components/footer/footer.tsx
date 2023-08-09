3839ce8fe3acbc5b1a7f6b9489c41919fb521925
Artem Islamov <artem.islamov@eigenmethod.com>
2023-08-09 14:38:25 +0100

    RTY-178 Change email address to info@rentality.xyz in all social networks

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,13 +5,12 @@ export default function Footer() {
   return (
     <footer className="pl-12 pr-4 py-8">
       <div className="flex flex-row">
-        <div className="flex flex-col w-1/2">
+        <div className="w-1/2 flex flex-col">
           <Image src={logo} alt="" />
         </div>
-        <div className="flex flex-col w-1/2">
+        <div className="w-1/2 flex flex-col gap-4">
           <div className="text-3xl font-semibold">Contacts</div>
-          <div className="text-2xl">+1 (888) 892-6789</div>
-          <div className="text-xl">info@rentality.us</div>
+          <div className="text-xl">info@rentality.xyz</div>
         </div>
       </div>
       <span>©2023 by Rentality LLC</span>
