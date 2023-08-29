12bbbc9b83c42d83317cb5e8a93edc40466e157a
Artem Islamov <artem.islamov@eigenmethod.com>
2023-08-29 10:15:37 +0100

    Add image.ts utils to resize images

--- src/pages/host/vehicles/add/index.tsx
+++ src/pages/host/vehicles/add/index.tsx
@@ -8,6 +8,7 @@ import CarEditForm from "@/components/host/carEditForm/carEditForm";
 import Link from "next/link";
 import RntDialogs from "@/components/common/rntDialogs";
 import useRntDialogs from "@/hooks/useRntDialogs";
+import { resizeImage } from "@/utils/image";
 
 export default function AddCar() {
   const [
@@ -24,46 +25,6 @@ export default function AddCar() {
   const [isButtonSaveDisabled, setIsButtonSaveDisabled] =
     useState<boolean>(false);
   const router = useRouter();
-
-  // const resizeImageToSquare = async (file: File): Promise<File> => {
-  //   return new Promise((resolve, reject) => {
-  //     const canvas = document.createElement("canvas");
-  //     const ctx = canvas.getContext("2d");
-  //     if (!ctx) return;
-  //     const img = new Image();
-  //     img.src = URL.createObjectURL(file);
-
-  //     img.onload = () => {
-  //       const size = 1000;
-  //       canvas.width = size;
-  //       canvas.height = size;
-  //       ctx.fillStyle = "transparent";
-  //       ctx.fillRect(0, 0, canvas.width, canvas.height);
-  //       const scaleFactor = size / Math.max(img.width, img.height);
-  //       const scaledWidth = img.width * scaleFactor;
-  //       const scaledHeight = img.height * scaleFactor;
-  //       ctx.drawImage(
-  //         img,
-  //         (size - scaledWidth) / 2,
-  //         (size - scaledHeight) / 2,
-  //         scaledWidth,
-  //         scaledHeight
-  //       );
-  //       canvas.toBlob(
-  //         (blob) => {
-  //           const resizedFile = new File([blob as BlobPart], file.name, {
-  //             type: "image/png",
-  //           });
-  //           resolve(resizedFile);
-  //         },
-  //         "image/png",
-  //         1
-  //       );
-  //     };
-
-  //     img.onerror = reject;
-  //   });
-  // };
   const [dialogState, showInfo, showError, showMessager, hideSnackbar] =
     useRntDialogs();
 
@@ -73,9 +34,9 @@ export default function AddCar() {
     }
 
     const file = e.target.files[0];
-    //const resizedImage = await resizeImageToSquare(file);
-    setImageFile(file);
-    
+    const resizedImage = await resizeImage(file, 1000);
+    setImageFile(resizedImage);
+
     var reader = new FileReader();
 
     reader.onload = function (event) {
@@ -85,7 +46,7 @@ export default function AddCar() {
       });
     };
 
-    reader.readAsDataURL(file);
+    reader.readAsDataURL(resizedImage);
   };
 
   const saveCar = async (e: React.MouseEvent<HTMLButtonElement>) => {
---
+++ src/utils/image.ts
@@ -0,0 +1,69 @@
+export const resizeImage = async (file: File, maxSize:number): Promise<File> => {
+  return new Promise((resolve, reject) => {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+    const img = new Image();
+    img.src = URL.createObjectURL(file);
+
+    img.onload = () => {
+      const scaleFactor = maxSize / Math.max(img.width, img.height);
+      const width = img.width * scaleFactor;
+      const height = img.height * scaleFactor;
+      canvas.width = width;
+      canvas.height = height;
+      ctx.drawImage(img, 0, 0, width, height);
+      canvas.toBlob(
+        (blob) => {
+          const resizedFile = new File([blob as BlobPart], file.name, {
+            type: "image/png",
+          });
+          resolve(resizedFile);
+        },
+        "image/png",
+        1
+      );
+    };
+
+    img.onerror = reject;
+  });
+};
+
+export const resizeImageToSquare = async (file: File, size:number): Promise<File> => {
+  return new Promise((resolve, reject) => {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+    const img = new Image();
+    img.src = URL.createObjectURL(file);
+
+    img.onload = () => {
+      canvas.width = size;
+      canvas.height = size;
+      ctx.fillStyle = "transparent";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      const scaleFactor = size / Math.max(img.width, img.height);
+      const scaledWidth = img.width * scaleFactor;
+      const scaledHeight = img.height * scaleFactor;
+      ctx.drawImage(
+        img,
+        (size - scaledWidth) / 2,
+        (size - scaledHeight) / 2,
+        scaledWidth,
+        scaledHeight
+      );
+      canvas.toBlob(
+        (blob) => {
+          const resizedFile = new File([blob as BlobPart], file.name, {
+            type: "image/png",
+          });
+          resolve(resizedFile);
+        },
+        "image/png",
+        1
+      );
+    };
+
+    img.onerror = reject;
+  });
+};
