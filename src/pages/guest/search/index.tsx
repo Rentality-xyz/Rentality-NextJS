ad52760aceb085a64fae9040d7d017cdada9292b
Dima Denisenko <dendima80@gmail.com>
2023-11-13 18:26:55 +0200

    switch for Guest and Host

--- src/pages/guest/search/index.tsx
+++ src/pages/guest/search/index.tsx
@@ -221,10 +221,7 @@ export default function Search() {
             <RntButton
                 className="w-40 max-xl:mt-4"
                 disabled={searchButtonDisabled}
-                onClick={() =>
-                    handleSearchClick()
-                  // showError("value should be more then 0 showMessager success value should be more then 0 showMessager success value should be more then 0 showMessager success")
-            }
+                onClick={() => handleSearchClick()}
             >
               Search
             </RntButton>
