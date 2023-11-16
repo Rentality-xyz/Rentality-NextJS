231cccfdaa7cf115529e90d9efe9d9bf3a541615
Artem Islamov <artem.islamov@eigenmethod.com>
2023-11-16 15:45:51 +0000

    Fix issue with civic control for local host network

--- src/components/CivicControl.tsx
+++ src/components/CivicControl.tsx
@@ -2,17 +2,27 @@ import { useEffect, useState } from "react";
 import { useRentality } from "../contexts/rentalityContext";
 import { Wallet } from "ethers";
 import { GatewayProvider } from "@civic/ethereum-gateway-react";
+import { DEFAULT_LOCAL_HOST_CHAIN_ID } from "@/utils/constants";
 
 export const CivicControl = ({ children }: { children?: React.ReactNode }) => {
   const gatekeeperNetwork = "uniqobk8oGh4XBLMqM68K8M2zNu3CdYX7q5go7whQiv";
   const [wallet, setWallet] = useState<Wallet>();
+  const [isLocalHost, setIsLocalHost] = useState<boolean>(true);
   const rentalityInfo = useRentality();
 
   useEffect(() => {
-    if (!rentalityInfo) return;
+    const setData = async () => {
+      if (!rentalityInfo) return;
 
-    setWallet(rentalityInfo.signer as Wallet);
+      const chainId = await rentalityInfo.signer.getChainId();
+      setIsLocalHost(chainId === DEFAULT_LOCAL_HOST_CHAIN_ID);
+      setWallet(rentalityInfo.signer as Wallet);
+    };
+
+    setData();
   }, [rentalityInfo]);
+
+  if (isLocalHost) return <>{children}</>;
 
   return (
     <GatewayProvider wallet={wallet} gatekeeperNetwork={gatekeeperNetwork}>
---
+++ src/utils/constants.ts
@@ -0,0 +1,1 @@
+export const DEFAULT_LOCAL_HOST_CHAIN_ID = 1337;
