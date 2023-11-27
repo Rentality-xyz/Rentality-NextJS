d4362a5f2a35c52db531fe8eb3312584cb7b7b1e
Dima Denisenko <dendima80@gmail.com>
2023-11-27 19:59:14 +0200

    Замечание 21 -> Мобильный интерфейс: слишком темновата подожка в постах. И дату поста почти не видно.

--- src/components/chat/chatMessage.tsx
+++ src/components/chat/chatMessage.tsx
@@ -11,18 +11,18 @@ type Props = {
 
 export default function ChatMessage({ photoUrl, name, datetime, message, isMyMessage }: Props) {
   return !isMyMessage ? (
-    <div className="bg-rentality-bg rnt-card-selected w-5/6 grid grid-cols-[auto_1fr_auto] gap-2 rounded-xl rounded-ss-none  overflow-hidden p-4">
+    <div className="bg-[#484874] rnt-card-selected w-5/6 grid grid-cols-[auto_1fr_auto] gap-2 rounded-xl rounded-ss-none  overflow-hidden p-4">
       <div className="w-12 h-12">
         <Avatar src={photoUrl} sx={{ width: "3rem", height: "3rem" }}></Avatar>
       </div>
-      <div className="font-bold text-lg self-center">{name}</div>
-      <div className="text-sm self-center text-gray-600">{dateFormat(datetime)}</div>
+      <div className="max-sm:leading-5 font-bold text-lg self-center">{name}</div>
+      <div className="text-sm self-center text-gray-400">{dateFormat(datetime)}</div>
       <div className="col-span-3 text-sm">{message}</div>
     </div>
   ) : (
     <div className="bg-[#7856FF] rnt-card w-5/6 grid grid-cols-[auto_1fr_auto] gap-2 rounded-xl rounded-se-none overflow-hidden p-4 self-end">
-      <div className="text-sm self-center text-gray-600">{dateFormat(datetime)}</div>
-      <div className="font-bold text-lg self-center text-end">{name}</div>
+      <div className="text-sm self-center text-gray-400">{dateFormat(datetime)}</div>
+      <div className="max-sm:leading-5 font-bold text-lg self-center text-end">{name}</div>
       <div className="w-12 h-12">
         <Avatar src={photoUrl} sx={{ width: "3rem", height: "3rem" }}></Avatar>
       </div>
