a199edf1ae0574f1f1f3f3a265f9fad7fed3c468
Artem Islamov <artem.islamov@eigenmethod.com>
2023-11-27 22:06:06 +0000

    fix admin layout error with app context

--- src/components/admin/layout/adminLayout.tsx
+++ src/components/admin/layout/adminLayout.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/components/footer/footer";
 import Header from "@/components/header/header";
 import AdminSideNavMenu from "@/components/sideNavMenu/adminSideNavMenu";
+import { AppContextProvider } from "@/hooks/useAppContext";
 
 type Props = {
   children?: React.ReactNode;
@@ -10,10 +11,12 @@ export default function AdminLayout({ children }: Props) {
   return (
     <>
       <div className="main-grid">
-        <Header accountType="Admin" />
-        <AdminSideNavMenu />
-        <main className="px-8 py-4">{children}</main>
-        <Footer />
+        <AppContextProvider>
+          <Header accountType="Admin" />
+          <AdminSideNavMenu />
+          <main className="px-8 py-4">{children}</main>
+          <Footer />
+        </AppContextProvider>
       </div>
     </>
   );
