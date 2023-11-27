fbc848be8fcc8e628b6d7b5b71cb6f8ba1edc3a5
Artem Islamov <artem.islamov@eigenmethod.com>
2023-11-27 19:45:01 +0000

    Fix statuss color issue

--- src/components/host/tripItem.tsx
+++ src/components/host/tripItem.tsx
@@ -63,7 +63,7 @@ export default function TripItem({ tripInfo, changeStatusCallback, disableButton
   };
   let statusBgColor = getTripStatusBgColorClassFromStatus(tripInfo.status);
   const statusClassName = twMerge(
-    "absolute right-0 top-2 px-8 py-2 rounded-l-3xl bg-purple-600 text-rnt-temp-status-text text-end",
+    "absolute right-0 top-2 px-8 py-2 rounded-l-3xl text-rnt-temp-status-text text-end",
     statusBgColor
   );
 
@@ -78,6 +78,9 @@ export default function TripItem({ tripInfo, changeStatusCallback, disableButton
             height={1000}
             className="h-full w-full object-cover"
           /> */}
+
+        {/* Empty span to generate tatilwind css colors for statuses */}
+        <span className="bg-yellow-600 bg-lime-500 bg-blue-600 bg-blue-800 bg-purple-600 bg-purple-800 bg-fuchsia-700 bg-red-500" />
         <div
           style={{ backgroundImage: `url(${tripInfo.image})` }}
           className="relative w-full 1xl:w-64 min-h-[12rem] md:min-h-[16rem] xl:min-h-[12rem] flex-shrink-0 bg-center bg-cover"
