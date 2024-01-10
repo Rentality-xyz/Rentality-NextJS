9ea0dbbfac70d2e56ccd1b20151aad2f4a407b9d
Artem Islamov <artem.islamov@eigenmethod.com>
2024-01-10 16:19:45 +0000

    Review check box control

--- src/components/common/checkbox.tsx
+++ src/components/common/checkbox.tsx
@@ -1,5 +1,5 @@
 import { twMerge } from "tailwind-merge";
-import { ChangeEventHandler, MouseEventHandler } from "react";
+import { ChangeEventHandler } from "react";
 
 type Props = {
   className?: string;
@@ -9,6 +9,24 @@ type Props = {
 };
 
 export default function Checkbox({ className, title, value, onChange, ...props }: Props) {
+  const c = twMerge("", className);
+  const checkBoxBgStyle = value ? "bg-rentality-primary" : "bg-white group-hover:bg-[#bbb]";
+  return (
+    <div className={c}>
+      <label className="group flex flex-row items-center cursor-pointer select-none">
+        <input className="hidden" type="checkbox" value={value ? 1 : 0} onChange={onChange} />
+        <span className={`relative w-10 h-10 shrink-0 mr-4 border-2 rounded-md ${checkBoxBgStyle}`}>
+          {value ? (
+            <span className="absolute top-1 right-2.5 w-3.5 h-6 shrink-0 border-white border-b-4 border-r-4 rotate-45"></span>
+          ) : null}
+        </span>
+        {title}
+      </label>
+    </div>
+  );
+}
+
+export function CheckboxOld({ className, title, value, onChange, ...props }: Props) {
   const c = twMerge("", className);
   return (
     <div className={c}>
