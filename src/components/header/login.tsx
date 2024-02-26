4c741c0e295b7c8667f8ad393de79a58c3c84247
Artem Islamov <artem.islamov@eigenmethod.com>
2024-02-26 13:29:14 +0000

    RTY-296 Дисконект з Метамаску

--- src/components/header/login.tsx
+++ src/components/header/login.tsx
@@ -4,12 +4,15 @@ import { formatAddress } from "@/utils/addressFormatters";
 import { Avatar } from "@mui/material";
 import { isEmpty } from "@/utils/string";
 import { usePrivy, useWallets } from "@privy-io/react-auth";
-import { memo } from "react";
+import { ElementRef, memo, useEffect, useRef, useState } from "react";
+import { assertIsNode } from "@/utils/react";
 
 function Login() {
   const { connectWallet, login, ready, authenticated, logout } = usePrivy();
   const userInfo = useUserInfo();
   const { wallets } = useWallets();
+  const [isShowMenu, setIsShowMenu] = useState(false);
+  const loginWrapperRef = useRef<ElementRef<"div">>(null);
 
   const userFullName = `${userInfo?.firstName ?? ""} ${userInfo?.lastName ?? ""}`;
   const userInitials =
@@ -18,6 +21,28 @@ function Login() {
           userInfo?.lastName?.slice(0, 1)?.toUpperCase() ?? ""
         }`
       : null;
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleOnScroll, true);
+    window.addEventListener("click", handleOnOutsideClick);
+
+    return () => {
+      window.removeEventListener("scroll", handleOnScroll, true);
+      window.removeEventListener("click", handleOnOutsideClick);
+    };
+  }, []);
+
+  const handleOnScroll = (event: Event) => {
+    if (event.target === document || event.target === document.documentElement || event.target === document.body) {
+      setIsShowMenu(false);
+    }
+  };
+
+  const handleOnOutsideClick = (event: MouseEvent) => {
+    assertIsNode(event.target);
+    if (loginWrapperRef.current?.contains(event.target)) return;
+    setIsShowMenu(false);
+  };
 
   if (!ready) return <></>;
 
@@ -36,7 +61,15 @@ function Login() {
     );
 
   return (
-    <div className="flex flex-row gap-4 ml-2 xl:ml-16 items-center cursor-pointer relative">
+    <div
+      className="flex flex-row gap-4 ml-2 xl:ml-16 items-center cursor-pointer relative"
+      ref={loginWrapperRef}
+      onClick={async () => {
+        setIsShowMenu((prev) => {
+          return !prev;
+        });
+      }}
+    >
       <div className=" flex-col hidden xl:flex">
         <div>{userFullName}</div>
         <div className="text-sm">{formatAddress(userInfo?.address ?? "")}</div>
@@ -44,6 +77,13 @@ function Login() {
       <Avatar className="w-20 h-20" alt={userFullName} src={userInfo?.profilePhotoUrl}>
         {userInitials}
       </Avatar>
+      {isShowMenu && (
+        <div className="absolute p-4 top-[5.25rem] left-auto right-0 z-50 bg-[#1E1E30] rounded-xl border-2 border-[#373737]">
+          <RntButton className="w-28 sm:w-48 h-10 text-sm sm:text-base" onClick={logout}>
+            Logout
+          </RntButton>
+        </div>
+      )}
     </div>
   );
 }
