7333b9100d7ca5211ef57f9bbc4005692563d275
Artem Islamov <artem.islamov@eigenmethod.com>
2024-02-28 11:48:14 +0000

    menu icons review (change to png, format, add some future icons

--- src/components/sideNavMenu/menuIcons.ts
+++ src/components/sideNavMenu/menuIcons.ts
@@ -1,11 +1,51 @@
+import { StaticImageData } from "next/image";
+import icBooked from "@/images/ic_booked.png";
+import icClaims from "@/images/ic_claims.png";
+import icHistory from "@/images/ic_history.png";
+import icLegal from "@/images/ic_legal.png";
+import icListings from "@/images/ic_listings.png";
+import icLogout from "@/images/ic_logout.png";
+import icMessages from "@/images/ic_messages.png";
+import icNotifications from "@/images/ic_notifications.png";
+import icProfileSettings from "@/images/ic_profile_settings.png";
+import icTransactionHistory from "@/images/ic_transaction_history.png";
+
 enum MenuIcons {
   Booked,
-  History,
-  Messages,
   Claims,
-  ProfileSettings,
-  Listings,
+  History,
   Legal,
+  Listings,
+  Logout,
+  Messages,
+  Notifications,
+  ProfileSettings,
+  TransactionHistory,
 }
+
+export const getImageForMenu = (state: MenuIcons): StaticImageData => {
+  switch (state) {
+    case MenuIcons.Booked:
+      return icBooked;
+    case MenuIcons.Claims:
+      return icClaims;
+    case MenuIcons.History:
+      return icHistory;
+    case MenuIcons.Legal:
+      return icLegal;
+    case MenuIcons.Listings:
+      return icListings;
+    case MenuIcons.Logout:
+      return icLogout;
+    case MenuIcons.Messages:
+      return icMessages;
+    case MenuIcons.Notifications:
+      return icNotifications;
+    case MenuIcons.ProfileSettings:
+      return icProfileSettings;
+    case MenuIcons.TransactionHistory:
+      return icTransactionHistory;
+  }
+};
 
 export default MenuIcons;
--- src/components/sideNavMenu/sideNavMenuItem.tsx
+++ src/components/sideNavMenu/sideNavMenuItem.tsx
@@ -1,42 +1,39 @@
 import Link from "next/link";
-import Image, {StaticImageData} from "next/image";
-import icBooked from "@/images/ic_booked.jpg";
-import icHistory from "@/images/ic_history.jpg";
-import icMessages from "@/images/ic_messages.jpg";
-import icProfileSettings from "@/images/ic_profile_settings.jpg";
-import icListings from "@/images/ic_listings.jpg";
-import icClaims from "@/images/ic_claims.jpg";
-import icLegal from "@/images/ic_legal.jpg";
-import MenuIcons from "@/components/sideNavMenu/menuIcons";
-import {useAppContext} from "@/contexts/appContext";
+import Image from "next/image";
+import MenuIcons, { getImageForMenu } from "@/components/sideNavMenu/menuIcons";
+import { useAppContext } from "@/contexts/appContext";
 
-export default function SideNavMenuItem({ text, href, icon, target = '_self'}: { text: string; href: string; icon: MenuIcons, target?: string }) {
+export default function SideNavMenuItem({
+  text,
+  href,
+  icon,
+  target = "_self",
+  onClick,
+}: {
+  text: string;
+  href: string;
+  icon: MenuIcons;
+  target?: string;
+  onClick?: () => {};
+}) {
   const { toggleBurgerMenu } = useAppContext();
 
-  const getImageForMenu = (state: MenuIcons): StaticImageData => {
-    switch (state) {
-      case MenuIcons.Booked:
-        return icBooked;
-      case MenuIcons.History:
-        return icHistory;
-      case MenuIcons.Messages:
-        return icMessages;
-      case MenuIcons.ProfileSettings:
-        return icProfileSettings;
-      case MenuIcons.Listings:
-        return icListings;
-      case MenuIcons.Claims:
-        return icClaims;
-      case MenuIcons.Legal:
-        return icLegal;
+  const handleOnclick = () => {
+    toggleBurgerMenu();
+    if (onClick) {
+      onClick();
     }
   };
 
   return (
     <div className="py-1 h-12">
-      <Link href={href} onClick={toggleBurgerMenu} className="flex" target={target}>
+      <Link href={href} onClick={handleOnclick} className="flex" target={target}>
         <Image src={getImageForMenu(icon)} width={30} height={30} alt="" className="mr-2" />
-        {(icon == MenuIcons.Listings || icon == MenuIcons.Claims) ? <span className="pt-2">{text}</span> : <span className="pt-0.5">{text}</span>}
+        {icon == MenuIcons.Listings || icon == MenuIcons.Claims ? (
+          <span className="pt-2">{text}</span>
+        ) : (
+          <span className="pt-0.5">{text}</span>
+        )}
       </Link>
     </div>
   );
