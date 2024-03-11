f2ec4fadaa37f44c8d0d11f3a59bc3eeb051b9c7
Artem Islamov <artem.islamov@eigenmethod.com>
2024-03-11 22:06:46 +0000

    fix empty pinata uri

--- src/utils/ipfsUtils.ts
+++ src/utils/ipfsUtils.ts
@@ -2,17 +2,23 @@ import { isEmpty } from "./string";
 
 export const getIpfsURIfromPinata = (pinataURI: string) => {
   if (isEmpty(pinataURI)) return "";
-  return "https://ipfs.io/ipfs/" + pinataURI.split("/").pop();
+  const fileHash = pinataURI.split("/").pop();
+  if (isEmpty(fileHash)) return "";
+  return "https://ipfs.io/ipfs/" + fileHash;
 };
 
 export const getPinataGatewayURIfromPinata = (pinataURI: string) => {
   if (isEmpty(pinataURI)) return "";
-  return "https://ivory-specific-mink-961.mypinata.cloud/ipfs/" + pinataURI.split("/").pop();
+  const fileHash = pinataURI.split("/").pop();
+  if (isEmpty(fileHash)) return "";
+  return "https://ivory-specific-mink-961.mypinata.cloud/ipfs/" + fileHash;
 };
 
 export const getMetaDataFromIpfs = async (tokenURI: string) => {
   //var ipfsURI = getPinataGatewayURIfromPinata(tokenURI);
   var ipfsURI = getIpfsURIfromPinata(tokenURI);
+  if (!ipfsURI) return "";
+
   try {
     const response = await fetch(ipfsURI, {
       headers: {
