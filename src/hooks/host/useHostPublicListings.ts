faf530899aa849a3fb7496ecf2599dc4dca69244
Artem Islamov <artem.islamov@eigenmethod.com>
2024-03-26 19:03:32 +0000

    use new method getCarsOfHost()

--- src/hooks/host/useHostPublicListings.ts
+++ src/hooks/host/useHostPublicListings.ts
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 import { BaseCarInfo } from "@/model/BaseCarInfo";
 import { getIpfsURIfromPinata, getMetaDataFromIpfs } from "@/utils/ipfsUtils";
 import { useRentality } from "@/contexts/rentalityContext";
-import { validateContractCarInfo } from "@/model/blockchain/schemas_utils";
-import { ContractCarInfo } from "@/model/blockchain/schemas";
+import { ContractPublicHostCarDTO } from "@/model/blockchain/schemas";
+import { validateContractPublicHostCarDTO } from "@/model/blockchain/schemas_utils";
 
 const useHostPublicListings = (hostAddress: string) => {
   const rentalityContract = useRentality();
@@ -16,41 +16,38 @@ const useHostPublicListings = (hostAddress: string) => {
 
       try {
         setIsLoading(true);
-        const hostPublicListingsView: ContractCarInfo[] = await rentalityContract.getAllCars();
+        const hostPublicListingsView: ContractPublicHostCarDTO[] = await rentalityContract.getCarsOfHost(hostAddress);
 
         const hostPublicListingsData =
           hostPublicListingsView.length === 0
             ? []
             : await Promise.all(
-                hostPublicListingsView
-                  .filter((i) => i.createdBy.toLowerCase() === hostAddress.toLowerCase())
-                  .map(async (i: ContractCarInfo, index) => {
-                    if (index === 0) {
-                      validateContractCarInfo(i);
-                    }
-                    const metadataURI = await rentalityContract.getCarMetadataURI(i.carId);
-                    const meta = await getMetaDataFromIpfs(metadataURI);
+                hostPublicListingsView.map(async (i: ContractPublicHostCarDTO, index) => {
+                  if (index === 0) {
+                    validateContractPublicHostCarDTO(i);
+                  }
+                  const meta = await getMetaDataFromIpfs(i.metadataURI);
 
-                    const pricePerDay = Number(i.pricePerDayInUsdCents) / 100;
-                    const securityDeposit = Number(i.securityDepositPerTripInUsdCents) / 100;
-                    const milesIncludedPerDay = Number(i.milesIncludedPerDay);
+                  const pricePerDay = Number(i.pricePerDayInUsdCents) / 100;
+                  const securityDeposit = Number(i.securityDepositPerTripInUsdCents) / 100;
+                  const milesIncludedPerDay = Number(i.milesIncludedPerDay);
 
-                    let item: BaseCarInfo = {
-                      carId: Number(i.carId),
-                      ownerAddress: i.createdBy.toString(),
-                      image: getIpfsURIfromPinata(meta.image),
-                      brand: meta.attributes?.find((x: any) => x.trait_type === "Brand")?.value ?? "",
-                      model: meta.attributes?.find((x: any) => x.trait_type === "Model")?.value ?? "",
-                      year: meta.attributes?.find((x: any) => x.trait_type === "Release year")?.value ?? "",
-                      licensePlate: meta.attributes?.find((x: any) => x.trait_type === "License plate")?.value ?? "",
-                      pricePerDay: pricePerDay,
-                      securityDeposit: securityDeposit,
-                      milesIncludedPerDay: milesIncludedPerDay,
-                      currentlyListed: i.currentlyListed,
-                      isEditable: false,
-                    };
-                    return item;
-                  })
+                  const item: BaseCarInfo = {
+                    carId: Number(i.carId),
+                    ownerAddress: hostAddress,
+                    image: getIpfsURIfromPinata(meta.image),
+                    brand: meta.attributes?.find((x: any) => x.trait_type === "Brand")?.value ?? "",
+                    model: meta.attributes?.find((x: any) => x.trait_type === "Model")?.value ?? "",
+                    year: meta.attributes?.find((x: any) => x.trait_type === "Release year")?.value ?? "",
+                    licensePlate: meta.attributes?.find((x: any) => x.trait_type === "License plate")?.value ?? "",
+                    pricePerDay: pricePerDay,
+                    securityDeposit: securityDeposit,
+                    milesIncludedPerDay: milesIncludedPerDay,
+                    currentlyListed: i.currentlyListed,
+                    isEditable: false,
+                  };
+                  return item;
+                })
               );
 
         setHostPublicListings(hostPublicListingsData);
