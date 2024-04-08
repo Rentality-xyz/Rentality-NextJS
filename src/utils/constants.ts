55a5fdf9430258603abea8469fb1de07572816b9
Artem Islamov <artem.islamov@eigenmethod.com>
2024-04-08 15:58:06 +0100

    formating

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const DEFAULT_LOCAL_HOST_CHAIN_ID = 1337;
-export const DEFAULT_GOOGLE_MAPS_SEARCH_ZOOM = 10; 
+export const DEFAULT_GOOGLE_MAPS_SEARCH_ZOOM = 10;
 export const DEFAULT_GOOGLE_MAPS_SEARCH_CENTER = {
-	lat: 25.802942, 
-	lng: -80.501527
+  lat: 25.802942,
+  lng: -80.501527,
 };
-export const GOOGLE_MAPS_MAP_ID = '70e109bf32920b0b';
+export const GOOGLE_MAPS_MAP_ID = "70e109bf32920b0b";
