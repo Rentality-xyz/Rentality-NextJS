155d832a0e087d959c59906a66e67fe3ff09e3eb
Dima Denisenko <dendima80@gmail.com>
2024-05-20 14:01:37 +0300

    Jira-RTY-487

--- src/components/tripCard/carPhotoWithStatus.tsx
+++ src/components/tripCard/carPhotoWithStatus.tsx
@@ -4,11 +4,32 @@ import { memo } from "react";
 import { twMerge } from "tailwind-merge";
 
 function CarPhotoWithStatus({ carImageUrl, tripStatus }: { carImageUrl: string; tripStatus: TripStatus }) {
-  let statusBgColor = getTripStatusBgColorClassFromStatus(tripStatus);
-  const statusClassName = twMerge(
-    "absolute max-w-[90%] right-0 top-2 px-8 py-2 rounded-l-3xl text-rnt-temp-status-text text-end",
-    statusBgColor
-  );
+
+    let statusBgColor = getTripStatusBgColorClassFromStatus(tripStatus);
+    let completedTripStatusTextFirstWord = '';
+    let completedTripStatusTextSecondPart = '';
+    let statusClassName = "absolute max-w-[90%] right-0 top-2 py-2 rounded-l-3xl text-rnt-temp-status-text text-end";
+    statusClassName = twMerge(
+        statusClassName,
+        statusBgColor
+    );
+
+    const tripStatusText = getTripStatusTextFromStatus(tripStatus)
+
+    if (tripStatus === TripStatus.CompletedWithoutGuestComfirmation) {
+        const [firstWord, ...rest] = tripStatusText.split(' ');
+        completedTripStatusTextFirstWord = firstWord;
+        completedTripStatusTextSecondPart = rest.join(' ');
+        statusClassName = twMerge(
+            statusClassName,
+            "pl-6 pr-2"
+        );
+    } else {
+        statusClassName = twMerge(
+            statusClassName,
+            "px-8"
+        );
+    }
 
   return (
     <>
@@ -27,9 +48,16 @@ function CarPhotoWithStatus({ carImageUrl, tripStatus }: { carImageUrl: string; 
         style={{ backgroundImage: `url(${carImageUrl})` }}
         className="relative w-full xl:w-64 min-h-[12rem] md:min-h-[16rem] xl:min-h-[12rem] flex-shrink-0 bg-center bg-cover"
       >
-        <div className={statusClassName}>
-          <strong>{`${getTripStatusTextFromStatus(tripStatus)}`}</strong>
-        </div>
+          {tripStatus == TripStatus.CompletedWithoutGuestComfirmation ? (
+              <div className={statusClassName}>
+                  <strong>{`${completedTripStatusTextFirstWord}`}</strong>
+                  <p className="text-sm">{`${completedTripStatusTextSecondPart}`}</p>
+              </div>
+          ) : (
+              <div className={statusClassName}>
+                  <strong>{`${tripStatusText}`}</strong>
+              </div>
+          )}
       </div>
     </>
   );
