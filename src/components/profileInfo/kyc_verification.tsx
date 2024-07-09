c496d05373943632ba340ac77296dc7aab189621
Artem Islamov <artem.islamov@eigenmethod.com>
2024-07-09 09:10:59 +0100

    Reverse RTY-482 Постановка Civik - подтверждение и оплата KYC

--- src/components/profileInfo/kyc_verification.tsx
+++ src/components/profileInfo/kyc_verification.tsx
@@ -1,23 +1,13 @@
 import { TFunction } from "@/utils/i18n";
-import RntButton from "../common/rntButton";
-import { useRntDialogs } from "@/contexts/rntDialogsContext";
 import RntDriverLicenseVerified from "../common/rntDriverLicenseVerified";
-import CustomCivicDialog from "../kyc/customCivicDialog";
+import { ButtonMode, IdentityButton } from "@civic/ethereum-gateway-react";
 
 export default function KycVerification({ t }: { t: TFunction }) {
-  const { showCustomDialog, hideDialogs } = useRntDialogs();
-
-  function handleButtonClick() {
-    showCustomDialog(<CustomCivicDialog handleCancelClick={hideDialogs} />);
-  }
-
   return (
     <div id="driver_license_verification" className="mt-1.5">
       <p>{t("pass_license_verif")}</p>
       <div className="flex mt-4 items-center gap-2 md:gap-6">
-        <RntButton type="button" onClick={handleButtonClick}>
-          Get Pass
-        </RntButton>
+        <IdentityButton mode={ButtonMode.LIGHT} className="civicButton" />
         <RntDriverLicenseVerified t={t} />
       </div>
     </div>
