04b11c38c1befa05062c0200440039a8ccea4bec
Artem Islamov <artem.islamov@eigenmethod.com>
2024-07-09 09:08:43 +0100

    move coinbase schemaId to env file

--- src/components/header/LoginBase.tsx
+++ src/components/header/LoginBase.tsx
@@ -54,7 +54,7 @@ function LoginBase() {
     <Identity
       className="bg-transparent flex-row-reverse gap-4"
       address={userInfo.address}
-      schemaId="0xf8b05c79f090979bf4a80270aba232dff11a10d9ca55c4f88de95317970f0de9"
+      schemaId={process.env.NEXT_PUBLIC_COINBASE_SCHEMA_ID ?? ""}
     >
       <Name className="text-white" />
       <Address className="text-white" />
