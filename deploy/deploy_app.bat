e147bba0d87dde12bd4deb25530ce5bd977b711c
Artem Islamov <artem.islamov@eigenmethod.com>
2024-07-11 11:13:19 +0100

    add deploy scripts for app and layer0 envs

---
+++ deploy/deploy_app.bat
@@ -0,0 +1,24 @@
+@echo off 
+
+setlocal
+call :setESC
+
+cd..
+call firebase projects:list
+set PROJECT_ID=rentality-app-428806
+echo:
+echo %ESC%[91mMAKE SURE THAT YOU HAVE UPDATED THE CONFIGURATION OF GOOGLE ANALYTICS!!!%ESC%[0m
+echo Deploying %ESC%[93m%PROJECT_ID%%ESC%[0m... Press any key to deploy ...
+pause >nul
+call firebase use %PROJECT_ID%
+call firebase deploy
+echo:
+echo %PROJECT_ID% deployed successfully
+pause
+
+:setESC
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+  set ESC=%%b
+  exit /B 0
+)
+exit /B 0
