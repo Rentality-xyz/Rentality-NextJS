ab334280ae0670ca9a8cf922b6a1ac5e74b3273d
Dima Denisenko <dendima80@gmail.com>
2024-07-24 15:59:49 +0300

    jira-RTY-661

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -82,7 +82,7 @@ export default function Header({ accountType }: { accountType: string }) {
           {isHost ? <HostBurgerNavMenu /> : <GuestBurgerNavMenu />}
         </div>
       )}
-      <header className="text-rnt-temp-header-text">
+      <header className="text-rnt-temp-header-text pt-1.5">
         <div className="flex w-full pl-2 pr-2 sm:pr-6 py-2 min-h-[7rem] justify-between">
           <div className="flex flex-row items-center">
             <Image
