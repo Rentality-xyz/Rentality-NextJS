39d4e2371c716c23a07c6321d02375415bdc3444
Artem Islamov <artem.islamov@eigenmethod.com>
2024-08-01 09:22:38 +0100

    RTY-676 Додати посилання на логотип,яке переадресовує на головну сторінку сайту

--- src/components/sideNavMenu/baseBurgerNavMenu.tsx
+++ src/components/sideNavMenu/baseBurgerNavMenu.tsx
@@ -20,7 +20,7 @@ export default function BaseBurgerNavMenu({
   return (
     <aside id="burger-menu" className="pl-14 pr-12 pt-8 bg-[#1E1E30] text-rnt-temp-sidemenu-text">
       <div className="w-40">
-        <Link href={"/"} onClick={handleOnClick}>
+        <Link href={"https://rentality.xyz/"} onClick={handleOnClick}>
           <Image alt="" width={200} height={200} src={logo} />
         </Link>
       </div>
--- src/components/sideNavMenu/baseSideNavMenu.tsx
+++ src/components/sideNavMenu/baseSideNavMenu.tsx
@@ -14,7 +14,7 @@ export default function BaseSideNavMenu({
   return (
     <aside id="main-menu" className="lg:min-w-[300px] pl-14 pr-12 pt-12 bg-rentality-bg-left-sidebar max-lg:hidden">
       <div className="w-40">
-        <Link href={"/"}>
+        <Link href={"https://rentality.xyz/"}>
           <Image alt="" width={200} height={200} src={logo} />
         </Link>
       </div>
