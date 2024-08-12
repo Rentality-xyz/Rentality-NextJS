593590539b331b89abbb422f126f73cd170a9c32
Artem Islamov <artem.islamov@eigenmethod.com>
2024-08-12 16:57:21 +0100

    Fix cloud function error

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 import { fileURLToPath } from "node:url";
 import createJiti from "jiti";
-const jiti = createJiti(fileURLToPath(import.meta.url));
+import fs from "fs";
 
-// Import env here to validate during build. Using jiti we can import .ts files :)
-jiti("./src/utils/env");
+if (fs.existsSync("./src/")) {
+  const jiti = createJiti(fileURLToPath(import.meta.url));
+  console.warn(`Checking environment variables...`);
+  // Import env here to validate during build. Using jiti we can import .ts files :)
+  jiti("./src/utils/env");
+}
 
 /** @type {import('next').NextConfig} */
 console.debug(`next.config.js -> reactStrictMode: ${process.env.NEXT_USE_STRICT_MODE !== "false"}`);
