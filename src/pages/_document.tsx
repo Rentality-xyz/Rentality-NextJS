b547165706c28ee1de74d8ca30395d0035989094
Dima Denisenko <dendima80@gmail.com>
2024-08-12 16:03:26 +0300

    jira-RTY-726 Hotjar

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 import Hotjar from '@hotjar/browser';
 
 export default function Document() {
-    const siteId = 4131425;
+    const siteId = 5086721;
     const hotjarVersion = 6;
 
     Hotjar.init(siteId, hotjarVersion);
