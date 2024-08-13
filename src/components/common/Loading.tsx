9da1e4d5b6936d9ad4e6395ceee951896af93c72
Artem Islamov <artem.islamov@eigenmethod.com>
2024-08-13 22:44:55 +0100

    Add  Loading.tsx  component

---
+++ src/components/common/Loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+function Loading() {
+  const { t } = useTranslation();
+  return (
+    <div className="mt-5 flex max-w-screen-xl flex-wrap justify-between text-center">{t("common.info.loading")}</div>
+  );
+}
+
+export default Loading;
