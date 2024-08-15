f4847d2ec60f07e96472fdab8762ed9ff2d67687
Artem Islamov <artem.islamov@eigenmethod.com>
2024-08-15 13:45:58 +0100

    fix disabled button background

--- src/components/common/rntButton.tsx
+++ src/components/common/rntButton.tsx
@@ -3,12 +3,12 @@ import React from "react";
 
 interface RntButtonProps extends React.ComponentPropsWithoutRef<"button"> {}
 
-export default function RntButton({ className, disabled, type, children, ...restProps }: RntButtonProps) {
-  const bgColor = disabled ? "bg-gray-500" : "buttonGradient";
-  const c = twMerge("h-12 w-56 rounded-full text-white text-lg " + bgColor, className);
+export default function RntButton({ className, children, ...restProps }: RntButtonProps) {
+  const bgColor = !className?.includes("bg-") ? "buttonGradient" : "";
+  const c = twMerge("h-12 w-56 rounded-full text-white text-lg disabled:bg-gray-500 " + bgColor, className);
 
   return (
-    <button className={c} type={type} disabled={disabled} {...restProps}>
+    <button className={c} {...restProps}>
       {children}
     </button>
   );
