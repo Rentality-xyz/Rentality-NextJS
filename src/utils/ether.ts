9c91cb710fd41d8b293144a69f3f4da4138bb79b
Artem Islamov <artem.islamov@eigenmethod.com>
2024-08-27 10:29:35 +0100

    Review signature

--- src/utils/ether.ts
+++ src/utils/ether.ts
@@ -1,4 +1,4 @@
-import { BrowserProvider, EventLog, Log } from "ethers";
+import { BrowserProvider, Eip1193Provider, EventLog, hexlify, Log, Signer } from "ethers";
 
 export function isEventLog(log: EventLog | Log): log is EventLog {
   return (log as EventLog).args !== undefined;
@@ -10,4 +10,19 @@ export async function tryGetEnsName(provider: BrowserProvider, walletAddress: st
   } catch {
     return null;
   }
+}
+
+export async function signMessage(signer: Signer, message: string | Uint8Array): Promise<string> {
+  return await signer.signMessage(message);
+}
+
+export async function signMessageWithProvider(
+  provider: Eip1193Provider,
+  walletAddress: string,
+  message: string | Uint8Array
+): Promise<string> {
+  return await provider.request({
+    method: "personal_sign",
+    params: [hexlify(message), walletAddress.toLowerCase()],
+  });
 }
