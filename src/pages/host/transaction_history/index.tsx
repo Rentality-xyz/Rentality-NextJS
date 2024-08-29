ec4920bab8bd32283e602d677bd89e0ad0459bf2
Artem Islamov <artem.islamov@eigenmethod.com>
2024-08-29 13:00:18 +0100

    RTY-422 При розширенні ~1025px елементи таблиці вилазять за неї; футер не на весь екран

--- src/pages/host/transaction_history/index.tsx
+++ src/pages/host/transaction_history/index.tsx
@@ -1,25 +1,16 @@
+import Layout from "@/components/layout/layout";
 import PageTitle from "@/components/pageTitle/pageTitle";
 import TransactionHistoryContent from "@/components/transaction_history/transactionHistoryContent";
-import Layout from "@/components/layout/layout";
-import useTransactionHistory from "@/hooks/transaction_history/useTransactionHistory";
 import { useTranslation } from "react-i18next";
 
 export default function TransactionHistory() {
-  const [isLoading, transactions] = useTransactionHistory(true);
   const { t } = useTranslation();
 
   return (
     <Layout>
       <div className="flex flex-col">
         <PageTitle title={t("transaction_history.title")} />
-        {/*TODO прибрати !*/}
-        {!isLoading ? (
-          <div className="mt-5 flex max-w-screen-xl flex-wrap justify-between text-center">
-            {t("common.info.loading")}
-          </div>
-        ) : (
-          <TransactionHistoryContent isHost={true} transactions={transactions} t={t} />
-        )}
+        <TransactionHistoryContent isHost={true} t={t} />
       </div>
     </Layout>
   );
