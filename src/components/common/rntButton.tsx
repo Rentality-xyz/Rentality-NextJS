58a824fa7ef484499dbf118215401e82a336d4dc
Artem Islamov <artem.islamov@eigenmethod.com>
2024-08-30 16:36:22 +0100

    Run formatter

--- src/components/common/rntButton.tsx
+++ src/components/common/rntButton.tsx
@@ -5,7 +5,7 @@ interface RntButtonProps extends React.ComponentPropsWithoutRef<"button"> {}
 
 export default function RntButton({ className, type, children, ...restProps }: RntButtonProps) {
   const bgColor = !className?.includes("bg-") ? "buttonGradient" : "";
-  type = type??"button";
+  type = type ?? "button";
   const c = cn("h-12 w-56 rounded-full text-white text-lg disabled:bg-gray-500 ", bgColor, className);
 
   return (
