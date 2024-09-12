edd3edcba4cc1e5c866ef7e84211bb4f26410823
FilippFromKR <protasovgleb290@gmail.com>
2024-09-12 14:01:37 +0300

    Sort test cars in mainnet

--- src/hooks/guest/useSearchCars.ts
+++ src/hooks/guest/useSearchCars.ts
@@ -21,11 +21,24 @@ export type SortOptions = {
 };
 export type SortOptionKey = keyof SortOptions;
 
-const useSearchCars = () => {
+const useSearchCars = (chainId: number) => {
   const ethereumInfo = useEthereum();
   const rentalityContract = useRentality();
   const [isLoading, setIsLoading] = useState<Boolean>(false);
   const [searchResult, setSearchResult] = useState<SearchCarsResult>(emptySearchCarsResult);
+
+  function sortByTestWallet(a: SearchCarInfo, b: SearchCarInfo, listOfTestWallets: string[]) {
+    let isAInTestWallets = listOfTestWallets.includes(a.ownerAddress);
+    let isBInTestWallets = listOfTestWallets.includes(b.ownerAddress);
+
+    if (isAInTestWallets && !isBInTestWallets) {
+      return 1;
+    } else if (!isAInTestWallets && isBInTestWallets) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
 
   const searchAvailableCars = async (request: SearchCarRequest, filters: SearchCarFilters) => {
     try {
@@ -88,7 +101,24 @@ const useSearchCars = () => {
 
       const availableCarsData = apiJson as SearchCarInfo[];
 
+      let listOfTestWallets: string[] = [];
+      let mainnetsOnPlatform = process.env["NEXT_PUBLIC_MAINNETS"];
+      if (mainnetsOnPlatform !== undefined) {
+        let result = mainnetsOnPlatform.split(",").find((chain) => chain === chainId.toString());
+        if (result !== undefined) {
+          let listOfTestWalletsEnv = process.env["NEXT_PUBLIC_TEST_WALLETS"];
+          if (listOfTestWalletsEnv !== undefined) {
+            listOfTestWallets = listOfTestWalletsEnv.split(",");
+            availableCarsData.sort((a, b) => sortByTestWallet(a, b, listOfTestWallets));
+          }
+        }
+      }
+
       for (const carInfoI of availableCarsData) {
+        if (listOfTestWallets.length > 0 && listOfTestWallets.includes(carInfoI.ownerAddress))
+          carInfoI.isTestCar = true;
+        else carInfoI.isTestCar = false;
+
         for (const carInfoJ of availableCarsData) {
           if (
             carInfoI.carId == carInfoJ.carId &&
@@ -106,6 +136,7 @@ const useSearchCars = () => {
         availableCarsData[0].highlighted = true;
       }
 
+      console.log("RES: ", availableCarsData);
       setSearchResult({
         searchCarRequest: request,
         searchCarFilters: filters,
