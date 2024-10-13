758dccc088089ac241828561db2044d9b05b659e
Artem Islamov <artem.islamov@eigenmethod.com>
2024-10-13 23:06:35 +0100

    RTY-803  Добавить плагин pixels by meta на сайт

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,6 +23,7 @@ import { AuthProvider } from "@/contexts/auth/authContext";
 import { env } from "@/utils/env";
 import Hotjar from "@hotjar/browser";
 import Layout from "@/components/layout/layout";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -56,6 +57,25 @@ export default function App({ Component, pageProps }: AppProps) {
                       <AppContextProvider>
                         <RntDialogsProvider>
                           <Layout>
+                            {/* Global Site Code Pixel - Facebook Pixel */}
+                            <Script
+                              id="fb-pixel"
+                              strategy="afterInteractive"
+                              dangerouslySetInnerHTML={{
+                                __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init',  '${env.NEXT_PUBLIC_FB_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `,
+                              }}
+                            />
                             <Component {...pageProps} />
                           </Layout>
                         </RntDialogsProvider>
