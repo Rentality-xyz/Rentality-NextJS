ce677a0b4687c08e819899839d3a077cc2bd3b15
Dima Denisenko <dendima80@gmail.com>
2024-10-28 15:21:31 +0200

    jira-RTY-630

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -5,7 +5,7 @@ import GuestSideNavMenu from "../sideNavMenu/guestSideNavMenu";
 import AdminSideNavMenu from "../sideNavMenu/adminSideNavMenu";
 import { useAppContext } from "@/contexts/appContext";
 import useUserMode, { isAdmin, isHost } from "@/hooks/useUserMode";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { DialogActions } from "@/utils/dialogActions";
 import { t } from "i18next";
 import { useRntDialogs } from "@/contexts/rntDialogsContext";
@@ -32,14 +32,16 @@ export default function Layout({ children }: { children?: React.ReactNode }) {
   );
 
   const { showDialog, hideDialogs } = useRntDialogs();
+  const [smallScreenDialogShown, setSmallScreenDialogShown] = useState(false);
 
   useEffect(() => {
     const checkScreenWidth = () => {
       const screenWidth = window.matchMedia("(max-width: 409px)");
 
-      if (screenWidth.matches) {
+      if (screenWidth.matches && !smallScreenDialogShown) {
         const action = <>{DialogActions.OK(hideDialogs)}</>;
         showDialog(t("common.info.small_screen"), action);
+        setSmallScreenDialogShown(true);
       }
     };
 
@@ -49,7 +51,7 @@ export default function Layout({ children }: { children?: React.ReactNode }) {
     return () => {
       window.removeEventListener("resize", checkScreenWidth);
     };
-  }, []);
+  }, [smallScreenDialogShown, showDialog, hideDialogs, t]);
 
   return (
     <>
