18a2e57815a0510b389f266d3c8ff877d1cc2d3d
Artem Islamov <artem.islamov@eigenmethod.com>
2024-10-31 17:31:04 +0000

    extract timezone fetch logic and delete usage of createTripRequest

---
+++ src/utils/searchMapper.ts
@@ -0,0 +1,33 @@
+import { SearchCarFilters, SearchCarRequest } from "@/model/SearchCarRequest";
+import moment from "moment";
+import { getBlockchainTimeFromDate } from "./formInput";
+import { ContractSearchCarParams } from "@/model/blockchain/schemas";
+import { emptyContractLocationInfo } from "@/model/blockchain/schemas_utils";
+
+export function formatSearchAvailableCarsContractRequest(
+  searchCarRequest: SearchCarRequest,
+  searchCarFilters: SearchCarFilters,
+  timeZoneId: string
+) {
+  const startCarLocalDateTime = moment.tz(searchCarRequest.dateFrom, timeZoneId).toDate();
+  const endCarLocalDateTime = moment.tz(searchCarRequest.dateTo, timeZoneId).toDate();
+  const contractDateFromUTC = getBlockchainTimeFromDate(startCarLocalDateTime);
+  const contractDateToUTC = getBlockchainTimeFromDate(endCarLocalDateTime);
+  const contractSearchCarParams: ContractSearchCarParams = {
+    country: searchCarRequest.searchLocation.country ?? "",
+    state: searchCarRequest.searchLocation.state ?? "",
+    city: searchCarRequest.searchLocation.city ?? "",
+    brand: searchCarFilters.brand ?? "",
+    model: searchCarFilters.model ?? "",
+    yearOfProductionFrom: BigInt(searchCarFilters.yearOfProductionFrom ?? 0),
+    yearOfProductionTo: BigInt(searchCarFilters.yearOfProductionTo ?? 0),
+    pricePerDayInUsdCentsFrom: BigInt((searchCarFilters.pricePerDayInUsdFrom ?? 0) * 100),
+    pricePerDayInUsdCentsTo: BigInt((searchCarFilters.pricePerDayInUsdTo ?? 0) * 100),
+    userLocation: {
+      ...emptyContractLocationInfo,
+      latitude: searchCarRequest.searchLocation.latitude.toFixed(6),
+      longitude: searchCarRequest.searchLocation.longitude.toFixed(6),
+    },
+  };
+  return { contractDateFromUTC, contractDateToUTC, contractSearchCarParams } as const;
+}
