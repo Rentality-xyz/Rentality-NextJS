d205470e2861e542a5d064639ef0f44071d8842a
Artem Islamov <artem.islamov@eigenmethod.com>
2024-11-06 12:35:14 +0000

    add  authContext extra logging

--- src/contexts/auth/authContext.tsx
+++ src/contexts/auth/authContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, useCallback, useContext, useEffect, useMemo, useState } from "react";
 import { useLogin, useLogout, usePrivy, useWallets } from "@privy-io/react-auth";
+import { bigIntReplacer } from "@/utils/json";
 
 interface useAuthInterface {
   isLoadingAuth: boolean;
@@ -56,6 +57,25 @@ export const AuthProvider = ({ children }: { children?: React.ReactNode }) => {
     setIsAuthenticated(isAuth);
     setIsLoadingAuth(!ready || !walletsReady);
   }, [ready, walletsReady, authenticated, wallets]);
+
+  useEffect(() => {
+    console.log(`AuthProvider usePrivy.ready has changed to ${ready}`);
+  }, [ready]);
+  useEffect(() => {
+    console.log(`AuthProvider usePrivy.authenticated has changed to ${authenticated}`);
+  }, [authenticated]);
+  useEffect(() => {
+    console.log(`AuthProvider useWallets.ready has changed to ${walletsReady}`);
+  }, [walletsReady]);
+  useEffect(() => {
+    console.log(`AuthProvider wallets has changed to ${JSON.stringify(wallets, bigIntReplacer, 2)}`);
+  }, [wallets]);
+  useEffect(() => {
+    console.log(`AuthProvider isLoadingAuth has changed to ${isLoadingAuth}`);
+  }, [isLoadingAuth]);
+  useEffect(() => {
+    console.log(`AuthProvider isAuthenticated has changed to ${isAuthenticated}`);
+  }, [isAuthenticated]);
 
   const value = useMemo(
     () => ({
