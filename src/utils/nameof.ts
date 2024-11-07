5f162de1515c57d16ffd606bc85ef4d57e0b276f
Artem Islamov <artem.islamov@eigenmethod.com>
2024-11-07 15:39:41 +0000

    review nameof util function

--- src/utils/nameof.ts
+++ src/utils/nameof.ts
@@ -1,5 +1,5 @@
-export function nameof<TObject>(obj: TObject, key: keyof TObject): string;
-export function nameof<TObject>(key: keyof TObject): string;
+export function nameof<TObject>(obj: TObject, key: Extract<keyof TObject, string>): string;
+export function nameof<TObject>(key: Extract<keyof TObject, string>): string;
 export function nameof(key1: any, key2?: any): any {
   return key2 ?? key1;
 }
