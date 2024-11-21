d92077d1cd10e77330cc980c75a7ce5c66142156
Dima Denisenko <dendima80@gmail.com>
2024-11-21 19:03:01 +0200

    jira-RTY-586

--- src/components/points/ReferralsAndPointsHistoryTable.tsx
+++ src/components/points/ReferralsAndPointsHistoryTable.tsx
@@ -15,19 +15,6 @@ type ReferralsAndPointsHistoryTableProps = {
 };
 
 export default function ReferralsAndPointsHistoryTable({ isLoading, data }: ReferralsAndPointsHistoryTableProps) {
-  const { t } = useTranslation();
-  const ReferralProgramDescriptions: { [key in RefferalProgram]: string } = {
-    [RefferalProgram.SetKYC]: t("referrals_and_point.referral_program.set_kyc"),
-    [RefferalProgram.PassCivic]: t("referrals_and_point.referral_program.pass_civic"),
-    [RefferalProgram.AddFirstCar]: t("referrals_and_point.referral_program.add_first_car"),
-    [RefferalProgram.AddCar]: t("referrals_and_point.referral_program.add_car"),
-    [RefferalProgram.CreateTrip]: t("referrals_and_point.referral_program.create_trip"),
-    [RefferalProgram.FinishTripAsHost]: t("referrals_and_point.referral_program.finish_trip_as_host"),
-    [RefferalProgram.FinishTripAsGuest]: t("referrals_and_point.referral_program.finish_trip_as_guest"),
-    [RefferalProgram.UnlistedCar]: t("referrals_and_point.referral_program.unlisted_car"),
-    [RefferalProgram.Daily]: t("referrals_and_point.referral_program.daily"),
-    [RefferalProgram.DailyListing]: t("referrals_and_point.referral_program.daily_listing"),
-  };
   const headerSpanClassName = "text-center font-semibold px-2 font-light text-sm";
   const rowSpanClassName = "px-2 h-12 w-1/3";
 
@@ -51,7 +38,7 @@ export default function ReferralsAndPointsHistoryTable({ isLoading, data }: Refe
         <tbody className="text-sm">
           {data.map((entry, index) => (
             <tr key={index} className="border-b-[2px] border-b-gray-500">
-              <td className={rowSpanClassName}>{ReferralProgramDescriptions[entry.method] || "Description"}</td>
+              <td className={rowSpanClassName}>{entry.methodDescriptions || "Description"}</td>
               <td className={cn("text-center text-gray-400", rowSpanClassName)}>
                 {dateFormatShortMonthDateTime(entry.date, UTC_TIME_ZONE_ID)}
               </td>
--- src/hooks/points/usePointsHistory.ts
+++ src/hooks/points/usePointsHistory.ts
@@ -4,14 +4,29 @@ import { Err, Ok, Result } from "@/model/utils/result";
 import useInviteLink from "@/hooks/useRefferalProgram";
 import { calculateDays, UTC_TIME_ZONE_ID } from "@/utils/date";
 import { getDateFromBlockchainTimeWithTZ } from "@/utils/formInput";
+import { useTranslation } from "react-i18next";
 
 export type ReferralHistoryInfo = {
   points: number;
   date: Date;
-  method: RefferalProgram;
+  methodDescriptions: string;
 };
 
 const usePointsHistory = () => {
+  const { t } = useTranslation();
+  const ReferralProgramDescriptions: { [key in RefferalProgram]: string } = {
+    [RefferalProgram.SetKYC]: t("referrals_and_point.referral_program.set_kyc"),
+    [RefferalProgram.PassCivic]: t("referrals_and_point.referral_program.pass_civic"),
+    [RefferalProgram.AddFirstCar]: t("referrals_and_point.referral_program.add_first_car"),
+    [RefferalProgram.AddCar]: t("referrals_and_point.referral_program.add_car"),
+    [RefferalProgram.CreateTrip]: t("referrals_and_point.referral_program.create_trip"),
+    [RefferalProgram.FinishTripAsHost]: t("referrals_and_point.referral_program.finish_trip_as_host"),
+    [RefferalProgram.FinishTripAsGuest]: t("referrals_and_point.referral_program.finish_trip_as_guest"),
+    [RefferalProgram.UnlistedCar]: t("referrals_and_point.referral_program.unlisted_car"),
+    [RefferalProgram.Daily]: t("referrals_and_point.referral_program.daily"),
+    [RefferalProgram.DailyListing]: t("referrals_and_point.referral_program.daily_listing"),
+  };
+
   const [
     inviteHash,
     points,
@@ -57,10 +72,24 @@ const usePointsHistory = () => {
         if (historyData) {
           const data: ReferralHistoryInfo[] = await Promise.all(
             historyData.map(async (historyDataDto) => {
+              const isOneTime = historyDataDto.oneTime;
+
+              const dynamicDescriptions: { [key: number]: string } = {
+                [RefferalProgram.FinishTripAsHost]: isOneTime
+                  ? t("referrals_and_point.referral_program.finish_trip_as_host_one_time")
+                  : t("referrals_and_point.referral_program.finish_trip_as_host"),
+                [RefferalProgram.FinishTripAsGuest]: isOneTime
+                  ? t("referrals_and_point.referral_program.finish_trip_as_guest_one_time")
+                  : t("referrals_and_point.referral_program.finish_trip_as_guest"),
+              };
+
+              const methodDescriptions =
+                dynamicDescriptions[historyDataDto.method] || ReferralProgramDescriptions[historyDataDto.method];
+
               return {
                 points: historyDataDto.points,
                 date: getDateFromBlockchainTimeWithTZ(historyDataDto.date, UTC_TIME_ZONE_ID),
-                method: historyDataDto.method,
+                methodDescriptions,
               };
             })
           );
