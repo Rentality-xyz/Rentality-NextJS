ef7cc67251f3275c179785a66710a2042e1401a0
Artem Islamov <artem.islamov@eigenmethod.com>
2024-12-17 12:14:01 +0000

    Review  middleware headers

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  // TODO check other options
-  // const cspHeader = `
-  // default-src 'self';
-  // script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-  // style-src 'self' 'nonce-${nonce}';
-  // img-src 'self' blob: data:;
-  // font-src 'self';
-  // object-src 'none';
-  // base-uri 'self';
-  // form-action 'self';
-  // frame-ancestors 'none';
-  // upgrade-insecure-requests;
-  // `
-
   const cspHeader = `
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
     frame-ancestors 'none'; 
+    upgrade-insecure-requests;
 `;
 
   // Replace newline characters and spaces
