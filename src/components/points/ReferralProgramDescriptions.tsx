7b1913419c3bb93bfb55dee1c85c6db10848f05b
Dima Denisenko <dendima80@gmail.com>
2024-12-23 10:15:32 +0200

    JIRA-RTY-896

--- src/components/points/ReferralProgramDescriptions.tsx
+++ src/components/points/ReferralProgramDescriptions.tsx
@@ -5,10 +5,10 @@ export const ReferralProgramDescription = (t: TFunction, program: RefferalProgra
   const descriptions: { [key in RefferalProgram]: string } = {
     [RefferalProgram.SetKYC]: t("referrals_and_point.referral_program.set_kyc"),
     [RefferalProgram.PassCivic]: t("referrals_and_point.referral_program.pass_civic"),
-    [RefferalProgram.AddFirstCar]: t("referrals_and_point.referral_program.add_first_car"),
+    // [RefferalProgram.AddFirstCar]: t("referrals_and_point.referral_program.add_first_car"),
     [RefferalProgram.AddCar]: t("referrals_and_point.referral_program.add_car"),
     [RefferalProgram.CreateTrip]: t("referrals_and_point.referral_program.create_trip"),
-    [RefferalProgram.FinishTripAsHost]: t("referrals_and_point.referral_program.finish_trip_as_host"),
+    // [RefferalProgram.FinishTripAsHost]: t("referrals_and_point.referral_program.finish_trip_as_host"),
     [RefferalProgram.FinishTripAsGuest]: t("referrals_and_point.referral_program.finish_trip_as_guest"),
     [RefferalProgram.UnlistedCar]: t("referrals_and_point.referral_program.unlisted_car"),
     [RefferalProgram.Daily]: t("referrals_and_point.referral_program.daily"),
