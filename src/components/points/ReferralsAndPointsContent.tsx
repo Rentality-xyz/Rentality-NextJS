dcd878823b092350c4a49ca388e1664de83b8e7f
Dima Denisenko <dendima80@gmail.com>
2025-01-06 16:13:27 +0200

    JIRA-586

--- src/components/points/ReferralsAndPointsContent.tsx
+++ src/components/points/ReferralsAndPointsContent.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import icStarPointsWhite from "@/images/ic_star_points_white.svg";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import ReferralsAndPointsOwnPoints from "@/components/points/ReferralsAndPointsOwnPoints";
 import ReferralsAndPointsLink from "@/components/points/ReferralsAndPointsLink";
 import ReferralsAndPointsHistory from "@/components/points/ReferralsAndPointsHistory";
 import ReferralsAndPointsFromYourReferrals from "@/components/points/ReferralsAndPointsFromYourReferrals";
+import useInviteLink from "@/hooks/useRefferalProgram";
 
 type ReferralsAndPointsContentProps = {
   isHost: boolean;
@@ -13,6 +14,7 @@ type ReferralsAndPointsContentProps = {
 
 export default function ReferralsAndPointsContent({ isHost }: ReferralsAndPointsContentProps) {
   const { t } = useTranslation();
+  const { points } = useInviteLink();
 
   return (
     <div className="">
@@ -20,7 +22,7 @@ export default function ReferralsAndPointsContent({ isHost }: ReferralsAndPoints
       <div className="mt-3 flex w-fit items-center rounded-lg border border-rentality-star-point px-3 py-2 font-['Montserrat',Arial,sans-serif]">
         <Image src={icStarPointsWhite} alt="" className="mr-1 h-[22px]" />
         <p>
-          Your Balance <span className="text-rentality-secondary">5 000</span> points
+          Your Balance <span className="text-rentality-secondary">{points}</span> points
         </p>
       </div>
       <div className="my-4 flex flex-col gap-4 xl:flex-row">
