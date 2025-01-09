9ab5b196108be4afa7b917f268a5f041cf5fb0a3
Dima Denisenko <dendima80@gmail.com>
2025-01-09 14:37:31 +0200

    JIRA-939

--- src/components/guest/carSearchItem.tsx
+++ src/components/guest/carSearchItem.tsx
@@ -1,7 +1,7 @@
 import { SearchCarInfo } from "@/model/SearchCarsResult";
 import RntButton from "../common/rntButton";
 import { Avatar } from "@mui/material";
-import React, { useMemo, useState } from "react";
+import React, { useMemo, useReducer } from "react";
 import { displayMoneyWith2Digits } from "@/utils/numericFormatters";
 import { getEngineTypeIcon, getEngineTypeString } from "@/model/EngineType";
 import Image from "next/image";
@@ -11,6 +11,13 @@ import RntInputTransparent from "@/components/common/rntInputTransparent";
 import carSeatsIcon from "@/images/car_seats.svg";
 import carTransmissionIcon from "@/images/car_transmission.svg";
 import { AboutCarIcon } from "@/components/createTrip/AboutCarIcon";
+import { getDiscountablePriceFromCarInfo, getNotDiscountablePriceFromCarInfo } from "@/utils/price";
+import { getPromoPrice, PromoActionType, reducer } from "@/features/promocodes/promoCode";
+import { PROMOCODE_MAX_LENGTH } from "@/utils/constants";
+import { useForm } from "react-hook-form";
+import { enterPromoFormSchema, EnterPromoFormValues } from "@/features/promocodes/models/enterPromoFormSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import useCheckPromo from "@/features/promocodes/hooks/useCheckPromo";
 
 type TFunction = (key: string, options?: { [key: string]: any }) => string;
 
@@ -24,7 +31,7 @@ export default function CarSearchItem({
   isGuestHasInsurance,
 }: {
   searchInfo: SearchCarInfo;
-  handleRentCarRequest: (carInfo: SearchCarInfo) => void;
+  handleRentCarRequest: (carInfo: SearchCarInfo, PromoCode?: string) => void;
   handleShowRequestDetails: (carInfo: SearchCarInfo) => void;
   disableButton: boolean;
   isSelected: boolean;
@@ -32,11 +39,6 @@ export default function CarSearchItem({
   isGuestHasInsurance: boolean;
 }) {
   const { t } = useTranslation();
-
-  const [formInputPromoCode, setFormInputPromoCode] = useState("");
-  function handlePromoCodeChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setFormInputPromoCode(e.target.value);
-  }
 
   const t_comp: TFunction = (name, options) => {
     return t("search_page.car_search_item." + name, options);
@@ -53,6 +55,23 @@ export default function CarSearchItem({
 
   const isDisplayInsurance = searchInfo.isInsuranceRequired && !isGuestHasInsurance;
   const insurancePriceTotal = isDisplayInsurance ? searchInfo.insurancePerDayPriceInUsd * searchInfo.tripDays : 0;
+
+  const [state, dispatch] = useReducer(reducer, { status: "NONE" });
+  const { checkPromo } = useCheckPromo();
+  const { register, handleSubmit } = useForm<EnterPromoFormValues>({
+    resolver: zodResolver(enterPromoFormSchema),
+  });
+  async function onFormSubmit(formData: EnterPromoFormValues) {
+    dispatch({ type: PromoActionType.LOADING });
+
+    const result = await checkPromo(formData.enteredPromo);
+
+    if (!result.ok) {
+      dispatch({ type: PromoActionType.ERROR });
+    } else {
+      dispatch({ type: PromoActionType.SUCCESS, payload: { code: formData.enteredPromo, value: result.value.value } });
+    }
+  }
 
   return (
     <div className={mainClasses} onClick={() => setSelected(searchInfo.carId)}>
@@ -156,9 +175,6 @@ export default function CarSearchItem({
                 <span className="max-md:ml-4">${displayMoneyWith2Digits(insurancePriceTotal)}</span>
               </div>
             )}
-            {/*<div className="grid grid-cols-2 md:h-full md:items-stretch">*/}
-            {/*  */}
-            {/*</div>*/}
           </div>
         </div>
         <div className="mt-6 flex items-center justify-around">
@@ -177,25 +193,35 @@ export default function CarSearchItem({
             inputClassName="text-center"
             type="text"
             placeholder="Promo Code"
-            value={formInputPromoCode}
-            onChange={handlePromoCodeChange}
+            {...register("enteredPromo", {
+              onChange: (e) => {
+                dispatch({ type: PromoActionType.RESET });
+
+                const value = e.target.value;
+                if (value !== undefined && typeof value === "string" && value.length >= PROMOCODE_MAX_LENGTH) {
+                  handleSubmit(async (data) => await onFormSubmit(data))();
+                }
+              },
+            })}
           />
           <RntButton
             className="w-full text-base max-sm:mt-4 sm:w-[65%]"
-            onClick={() => handleRentCarRequest(searchInfo)}
-            disabled={disableButton}
+            onClick={() => handleRentCarRequest(searchInfo, state.status === "SUCCESS" ? state.promo.code : undefined)}
+            disabled={disableButton || state.status === "LOADING"}
           >
-            <div>
-              {t_comp("rent_for", { days: searchInfo.tripDays })} {t_comp("total")} $
-              {displayMoneyWith2Digits(
-                searchInfo.totalPriceWithDiscount +
-                  searchInfo.taxes +
-                  searchInfo.securityDeposit +
-                  searchInfo.deliveryDetails.pickUp.priceInUsd +
-                  searchInfo.deliveryDetails.dropOff.priceInUsd +
-                  insurancePriceTotal
-              )}
-            </div>
+            {state.status === "SUCCESS"
+              ? t("promo.button_promo_text", {
+                  price: displayMoneyWith2Digits(
+                    getPromoPrice(getDiscountablePriceFromCarInfo(searchInfo), state.promo.value) +
+                      getNotDiscountablePriceFromCarInfo(searchInfo)
+                  ),
+                })
+              : t("promo.button_default_text", {
+                  days: searchInfo.tripDays,
+                  price: displayMoneyWith2Digits(
+                    getDiscountablePriceFromCarInfo(searchInfo) + getNotDiscountablePriceFromCarInfo(searchInfo)
+                  ),
+                })}
           </RntButton>
         </div>
       </div>
