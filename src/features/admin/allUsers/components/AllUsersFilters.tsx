fe44981d9d30140a34baf592783c4ee5bda98147
Artem Islamov <artem.islamov@eigenmethod.com>
2025-01-10 14:14:18 +0000

    Add adminAllUsers base conponents (stop doing it because requirements were changed)

---
+++ src/features/admin/allUsers/components/AllUsersFilters.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { AdminAllUsersFilters } from "../hooks/useAdminAllUsers";
+
+interface AllUsersFiltersProps {
+  defaultFilters?: AdminAllUsersFilters;
+  onApply: (filters: AdminAllUsersFilters) => Promise<void>;
+}
+
+function AllUsersFilters({ defaultFilters, onApply }: AllUsersFiltersProps) {
+  return <div>AllUsersFilters</div>;
+}
+
+export default AllUsersFilters;
