afc1f6a058890a94cf80f6bf297a79eed825f232
Dima Denisenko <dendima80@gmail.com>
2025-01-10 10:14:59 +0200

    crop photo

--- src/components/guest/carSearchItem.tsx
+++ src/components/guest/carSearchItem.tsx
@@ -91,133 +91,135 @@ export default function CarSearchItem({
   }
 
   return (
-    <div className={mainClasses} onClick={() => setSelected(searchInfo.carId)}>
-      <div
-        style={{ backgroundImage: `url(${searchInfo.images[0]})` }}
-        className="relative min-h-[15rem] w-full flex-shrink-0 bg-cover bg-center md:min-h-[20rem]"
-      >
-        {searchInfo.isCarDetailsConfirmed && (
-          <i className="fi fi-br-hexagon-check absolute right-2 top-2 text-3xl text-green-500"></i>
-        )}
-        <Link href={requestDetailsLink} target="_blank">
-          <div className={"h-full w-full"} />
-        </Link>
-      </div>
+    <div className={mainClasses}>
+      <Link href={requestDetailsLink} target="_blank">
+        <div
+          style={{ backgroundImage: `url(${searchInfo.images[0]})` }}
+          className="relative min-h-[15rem] w-full flex-shrink-0 bg-cover bg-center md:min-h-[20rem]"
+        >
+          {searchInfo.isCarDetailsConfirmed && (
+            <i className="fi fi-br-hexagon-check absolute right-2 top-2 text-3xl text-green-500"></i>
+          )}
+        </div>
+      </Link>
       <div className="flex w-full flex-col justify-between p-2 sm:pb-2 sm:pl-4 sm:pr-2 sm:pt-2">
-        <div className="flex flex-row items-center justify-between">
-          <div className="w-full overflow-hidden max-sm:mr-2">
-            <strong className="truncate text-lg">{`${searchInfo.brand} ${searchInfo.model} ${searchInfo.year}`}</strong>
-          </div>
-          <div className="flex items-center">
-            <div className="mr-2 truncate text-lg font-medium">{searchInfo.hostName ?? "-"}</div>
-            <div className="h-12 w-12 self-center">
-              <Avatar src={searchInfo.hostPhotoUrl} sx={{ width: "3rem", height: "3rem" }}></Avatar>
+        <div onClick={() => setSelected(searchInfo.carId)}>
+          <div className="flex flex-row items-center justify-between">
+            <div className="w-full overflow-hidden max-sm:mr-2">
+              <strong className="truncate text-lg">{`${searchInfo.brand} ${searchInfo.model} ${searchInfo.year}`}</strong>
             </div>
-          </div>
-        </div>
-        <div className="mt-2 flex w-full flex-col text-sm md:grid md:grid-cols-[40%_1fr_1fr] md:gap-4">
-          <div className="relative flex flex-col">
-            {isNaN(searchInfo.pricePerDayWithHostDiscount) ||
-            searchInfo.pricePerDayWithHostDiscount === searchInfo.pricePerDay ? (
-              <div className="text-base">
-                <strong>
-                  ${displayMoneyWith2Digits(searchInfo.pricePerDay)}
-                  {t_comp("per_day")}
-                </strong>
+            <div className="flex items-center">
+              <div className="mr-2 truncate text-lg font-medium">{searchInfo.hostName ?? "-"}</div>
+              <div className="h-12 w-12 self-center">
+                <Avatar src={searchInfo.hostPhotoUrl} sx={{ width: "3rem", height: "3rem" }}></Avatar>
               </div>
-            ) : (
-              <div className="text-base">
-                <strong>
-                  ${displayMoneyWith2Digits(searchInfo.pricePerDayWithHostDiscount)}
-                  {t_comp("per_day")}
-                </strong>
-                <strong className="ml-8 text-[#8B8B8F] line-through">
-                  ${displayMoneyWith2Digits(searchInfo.pricePerDay)}
-                  {t_comp("per_day")}
-                </strong>
-              </div>
-            )}
-
-            <div className="mt-4 flex justify-between md:grid md:grid-cols-2">
-              <div>
-                <span>${displayMoneyWith2Digits(searchInfo.pricePerDay)}</span>
-                <span className="mx-0.5">x</span>
-                <span>
-                  {searchInfo.tripDays} {t_comp("days")}
-                </span>
-              </div>
-              <span className="ml-8">${displayMoneyWith2Digits(searchInfo.pricePerDay * searchInfo.tripDays)}</span>
             </div>
-
-            <div className="flex justify-between md:grid md:grid-cols-2">
-              <span className="text-rentality-secondary">{searchInfo.daysDiscount}</span>
-              <span className="ml-8 text-rentality-secondary">{searchInfo.totalDiscount}</span>
-            </div>
-
-            <div className="flex justify-between md:grid md:grid-cols-2">
-              <span>{t_comp("price_without_taxes")}</span>
-              <span className="ml-8">${displayMoneyWith2Digits(searchInfo.totalPriceWithHostDiscount)}</span>
-            </div>
-
-            {isDisplayInsurance && <p className="mt-2 text-rentality-secondary">{t_comp("insurance_required")}</p>}
-            <div className={cn("right-[10px] fullHD:right-[22px]", ccsDivider)}></div>
           </div>
-          <div className="relative flex flex-col justify-between max-md:mt-2">
-            <div className="flex justify-between md:grid md:grid-cols-2">
-              <span>{t_comp("delivery_fee_pick_up")}</span>
-              <span className="max-md:ml-4">
-                ${displayMoneyWith2Digits(searchInfo.deliveryDetails.pickUp.priceInUsd)}
-              </span>
-            </div>
+          <div className="mt-2 flex w-full flex-col text-sm md:grid md:grid-cols-[40%_1fr_1fr] md:gap-4">
+            <div className="relative flex flex-col">
+              {isNaN(searchInfo.pricePerDayWithHostDiscount) ||
+              searchInfo.pricePerDayWithHostDiscount === searchInfo.pricePerDay ? (
+                <div className="text-base">
+                  <strong>
+                    ${displayMoneyWith2Digits(searchInfo.pricePerDay)}
+                    {t_comp("per_day")}
+                  </strong>
+                </div>
+              ) : (
+                <div className="text-base">
+                  <strong>
+                    ${displayMoneyWith2Digits(searchInfo.pricePerDayWithHostDiscount)}
+                    {t_comp("per_day")}
+                  </strong>
+                  <strong className="ml-8 text-[#8B8B8F] line-through">
+                    ${displayMoneyWith2Digits(searchInfo.pricePerDay)}
+                    {t_comp("per_day")}
+                  </strong>
+                </div>
+              )}
 
-            <div className="flex justify-between md:grid md:grid-cols-2">
-              <span>{t_comp("delivery_fee_drop_off")}</span>
-              <span className="max-md:ml-4">
-                ${displayMoneyWith2Digits(searchInfo.deliveryDetails.dropOff.priceInUsd)}
-              </span>
-            </div>
+              <div className="mt-4 flex justify-between md:grid md:grid-cols-2">
+                <div>
+                  <span>${displayMoneyWith2Digits(searchInfo.pricePerDay)}</span>
+                  <span className="mx-0.5">x</span>
+                  <span>
+                    {searchInfo.tripDays} {t_comp("days")}
+                  </span>
+                </div>
+                <span className="ml-8">${displayMoneyWith2Digits(searchInfo.pricePerDay * searchInfo.tripDays)}</span>
+              </div>
 
-            <div className="flex justify-between md:grid md:grid-cols-2">
-              <span>{t_comp("taxes")}</span>
-              <span className="max-md:ml-4">${displayMoneyWith2Digits(searchInfo.taxes)}</span>
-            </div>
-
-            <div className="flex justify-between md:grid md:grid-cols-2">
-              <span>{t_comp("deposit")}</span>
-              <span className="max-md:ml-4">${displayMoneyWith2Digits(searchInfo.securityDeposit)}</span>
-            </div>
-
-            {isDisplayInsurance && (
               <div className="flex justify-between md:grid md:grid-cols-2">
-                <span>
-                  <Image src={getImageForMenu(MenuIcons.Insurance)} width={20} height={20} alt="" />
-                </span>
-                <span className="max-md:ml-4">${displayMoneyWith2Digits(insurancePriceTotal)}</span>
+                <span className="text-rentality-secondary">{searchInfo.daysDiscount}</span>
+                <span className="ml-8 text-rentality-secondary">{searchInfo.totalDiscount}</span>
               </div>
-            )}
-            <div className={cn("right-[2px]", ccsDivider)}></div>
-          </div>
-          <div className="flex flex-col items-center">
-            <div className="flex justify-between max-md:mt-6 max-md:w-full md:h-full md:flex-col">
-              <AboutCarIcon
-                className="grid-cols-2 md:grid"
-                image={carSeatsIcon}
-                text={`${searchInfo.seatsNumber} ${t_comp("seats")}`}
-              />
-              <AboutCarIcon
-                className="grid-cols-2 md:grid"
-                image={getEngineTypeIcon(searchInfo.engineType)}
-                width={50}
-                text={getEngineTypeString(searchInfo.engineType)}
-              />
-              <AboutCarIcon
-                className="grid-cols-2 md:grid"
-                image={carTransmissionIcon}
-                text={searchInfo.transmission}
-              />
+
+              <div className="flex justify-between md:grid md:grid-cols-2">
+                <span>{t_comp("price_without_taxes")}</span>
+                <span className="ml-8">${displayMoneyWith2Digits(searchInfo.totalPriceWithHostDiscount)}</span>
+              </div>
+
+              {isDisplayInsurance && <p className="mt-2 text-rentality-secondary">{t_comp("insurance_required")}</p>}
+              <div className={cn("right-[10px] fullHD:right-[22px]", ccsDivider)}></div>
+            </div>
+            <div className="relative flex flex-col justify-between max-md:mt-2">
+              <div className="flex justify-between md:grid md:grid-cols-2">
+                <span>{t_comp("delivery_fee_pick_up")}</span>
+                <span className="max-md:ml-4">
+                  ${displayMoneyWith2Digits(searchInfo.deliveryDetails.pickUp.priceInUsd)}
+                </span>
+              </div>
+
+              <div className="flex justify-between md:grid md:grid-cols-2">
+                <span>{t_comp("delivery_fee_drop_off")}</span>
+                <span className="max-md:ml-4">
+                  ${displayMoneyWith2Digits(searchInfo.deliveryDetails.dropOff.priceInUsd)}
+                </span>
+              </div>
+
+              <div className="flex justify-between md:grid md:grid-cols-2">
+                <span>{t_comp("taxes")}</span>
+                <span className="max-md:ml-4">${displayMoneyWith2Digits(searchInfo.taxes)}</span>
+              </div>
+
+              <div className="flex justify-between md:grid md:grid-cols-2">
+                <span>{t_comp("deposit")}</span>
+                <span className="max-md:ml-4">${displayMoneyWith2Digits(searchInfo.securityDeposit)}</span>
+              </div>
+
+              {isDisplayInsurance && (
+                <div className="flex justify-between md:grid md:grid-cols-2">
+                  <span>
+                    <Image src={getImageForMenu(MenuIcons.Insurance)} width={20} height={20} alt="" />
+                  </span>
+                  <span className="max-md:ml-4">${displayMoneyWith2Digits(insurancePriceTotal)}</span>
+                </div>
+              )}
+              <div className={cn("right-[2px]", ccsDivider)}></div>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="flex justify-between max-md:mt-6 max-md:w-full md:h-full md:flex-col">
+                <AboutCarIcon
+                  className="grid-cols-2 md:grid"
+                  image={carSeatsIcon}
+                  text={`${searchInfo.seatsNumber} ${t_comp("seats")}`}
+                />
+                <AboutCarIcon
+                  className="grid-cols-2 md:grid"
+                  image={getEngineTypeIcon(searchInfo.engineType)}
+                  width={50}
+                  text={getEngineTypeString(searchInfo.engineType)}
+                />
+                <AboutCarIcon
+                  className="grid-cols-2 md:grid"
+                  image={carTransmissionIcon}
+                  text={searchInfo.transmission}
+                />
+              </div>
             </div>
           </div>
         </div>
+
         <div className="mt-6 flex w-full max-sm:flex-col">
           <RntInputTransparent
             className="mr-2 w-full sm:w-[35%]"
