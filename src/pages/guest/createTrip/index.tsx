6186514c7a1c354c3c10580b9635abb80e7ba069
Artem Islamov <artem.islamov@eigenmethod.com>
2025-01-11 09:56:26 +0000

    return 0 on 100% promo

--- src/pages/guest/createTrip/index.tsx
+++ src/pages/guest/createTrip/index.tsx
@@ -281,8 +281,9 @@ function CreateTripDetailsContent({
           {state.status === "SUCCESS"
             ? t("promo.button_promo_text", {
                 price: displayMoneyWith2Digits(
-                  getPromoPrice(getDiscountablePriceFromCarInfo(carInfo), state.promo.value) +
-                    getNotDiscountablePriceFromCarInfo(carInfo)
+                  getPromoPrice(getDiscountablePriceFromCarInfo(carInfo), state.promo.value) + state.promo.value !== 100
+                    ? getNotDiscountablePriceFromCarInfo(carInfo)
+                    : 0
                 ),
               })
             : t("promo.button_default_text", {
