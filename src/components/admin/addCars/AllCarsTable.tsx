607bf733ca98130b4ef7a545181cc22ea8498c6b
danyloday <danil.vladyka1@gmail.com>
2025-01-20 21:18:00 +0200

    RTY-866: Added table support for mobile

--- src/components/admin/addCars/AllCarsTable.tsx
+++ src/components/admin/addCars/AllCarsTable.tsx
@@ -31,33 +31,34 @@ export default function AllCarsTable({ isLoading, data, checkVin }: AllCarsTable
         </div>
       }
     >
-      <div className="text-xl lg:hidden">{t("common.low_resolution")}</div>
-      <table className="hidden w-full table-auto border-spacing-2 overflow-x-auto lg:block">
-        <thead className="mb-2">
-          <tr className="border-b-[2px] border-b-gray-500">
-            <th className={`${cn(headerSpanClassName, "min-w-[6ch]")}`}>#</th>
-            <th className={`${cn(headerSpanClassName, "min-w-[6ch]")}`}>{t_comp("carId")}</th>
-            <th className={`${cn(headerSpanClassName, "min-w-[15ch]")}`}>{t_comp("host")}</th>
-            <th className={`${cn(headerSpanClassName, "min-w-[10ch]")}`}>{t_comp("status")}</th>
-            <th className={`${cn(headerSpanClassName, "min-w-[10ch]")}`}>{t_comp("image")}</th>
-            <th
-              className={`${cn(headerSpanClassName, "min-w-[25ch]")}`}
-            >{`${t_comp("country")}/${t_comp("state")}\n${t_comp("city")}`}</th>
-            <th className={`${cn(headerSpanClassName, "min-w-[20ch]")}`}>
-              {`${t_comp("latitude")}\n${t_comp("longitude")}\n${t_comp("timeZoneId")}`}
-            </th>
-            <th className={`${cn(headerSpanClassName, "min-w-[20ch]")}`}>{t_comp("homeAddress")}</th>
-            <th className={`${cn(headerSpanClassName, "min-w-[20ch]")}`}>{t_comp("checkVin")}</th>
-          </tr>
-        </thead>
-        <tbody className="text-sm">
-          {data.map((carDetails, index) => {
-            return (
-              <AllCarsTableRow key={carDetails.vinNumber} index={index} carDetails={carDetails} checkVin={checkVin} />
-            );
-          })}
-        </tbody>
-      </table>
+      <div className="w-full overflow-x-auto">
+        <table className="w-full table-auto border-spacing-2">
+          <thead className="mb-2">
+            <tr className="border-b-[2px] border-b-gray-500">
+              <th className={`${cn(headerSpanClassName, "min-w-[6ch]")}`}>#</th>
+              <th className={`${cn(headerSpanClassName, "min-w-[6ch]")}`}>{t_comp("carId")}</th>
+              <th className={`${cn(headerSpanClassName, "min-w-[15ch]")}`}>{t_comp("host")}</th>
+              <th className={`${cn(headerSpanClassName, "min-w-[10ch]")}`}>{t_comp("status")}</th>
+              <th className={`${cn(headerSpanClassName, "min-w-[10ch]")}`}>{t_comp("image")}</th>
+              <th
+                className={`${cn(headerSpanClassName, "min-w-[25ch]")}`}
+              >{`${t_comp("country")}/${t_comp("state")}\n${t_comp("city")}`}</th>
+              <th className={`${cn(headerSpanClassName, "min-w-[20ch]")}`}>
+                {`${t_comp("latitude")}\n${t_comp("longitude")}\n${t_comp("timeZoneId")}`}
+              </th>
+              <th className={`${cn(headerSpanClassName, "min-w-[20ch]")}`}>{t_comp("homeAddress")}</th>
+              <th className={`${cn(headerSpanClassName, "min-w-[20ch]")}`}>{t_comp("checkVin")}</th>
+            </tr>
+          </thead>
+          <tbody className="text-sm">
+            {data.map((carDetails, index) => {
+              return (
+                <AllCarsTableRow key={carDetails.vinNumber} index={index} carDetails={carDetails} checkVin={checkVin} />
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </RntSuspense>
   );
 }
