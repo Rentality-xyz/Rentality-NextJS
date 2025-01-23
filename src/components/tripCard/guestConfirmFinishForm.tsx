1bf4ac551c0a9385747812c05eed918b1a1ba9d2
Vlad Stulikov <vladstulikov@gmail.com>
2025-01-22 19:02:37 -0500

    RTY-340

--- src/components/tripCard/guestConfirmFinishForm.tsx
+++ src/components/tripCard/guestConfirmFinishForm.tsx
@@ -79,6 +79,7 @@ function GuestConfirmFinishForm({
             isHost={false}
             isStart={false}
             tripId={tripId}
+            isSimpleButton={true}
           />
         </>
       )}
