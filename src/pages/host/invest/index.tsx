cefcc30616d07424444e607d8643e2f93b847b89
Dima Denisenko <dendima80@gmail.com>
2025-01-23 13:50:32 +0200

    jira 458 invest

--- src/pages/host/invest/index.tsx
+++ src/pages/host/invest/index.tsx
@@ -2,7 +2,6 @@ import PageTitle from "@/components/pageTitle/pageTitle";
 import { useTranslation } from "react-i18next";
 import React from "react";
 import CheckingLoadingAuth from "@/components/common/CheckingLoadingAuth";
-import ReferralsAndPointsContent from "@/components/points/ReferralsAndPointsContent";
 import InvestContent from "@/components/invest/InvestContent";
 
 export default function HostInvest() {
