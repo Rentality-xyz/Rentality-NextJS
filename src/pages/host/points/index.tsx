84e3bc570897473edd07088ff33f1bde0e10d6b2
Dima Denisenko <dendima80@gmail.com>
2025-01-23 13:36:28 +0200

    merge fix

--- src/pages/host/points/index.tsx
+++ src/pages/host/points/index.tsx
@@ -1,9 +1,4 @@
-import { useTranslation } from "react-i18next";
-import React from "react";
 import ReferralPageContent from "@/features/referralProgram/pages/ReferralPageContent";
-
-export default function ReferralsAndPoints() {
-  const { t } = useTranslation();
 
 function ReferralsAndPoints() {
   return <ReferralPageContent />;
