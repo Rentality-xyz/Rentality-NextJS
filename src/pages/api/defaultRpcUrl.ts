c2b552c00eeb0ddb1f16883cf322ba59c9fface7
Artem Islamov <artem.islamov@eigenmethod.com>
2025-01-27 17:12:42 +0000

    Delete unused methods in  IRentalityGateway and change  defaultRpcUrl call from POST to GET

--- src/pages/api/defaultRpcUrl.ts
+++ src/pages/api/defaultRpcUrl.ts
@@ -3,40 +3,37 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { ApiUrl } from "./publicSearchCars";
 
 type QueryParams = {
-    chainId: string; 
-  };
-  
-export default async function handler(req: NextApiRequest & { query: QueryParams }, res: NextApiResponse<ApiUrl>) {
+  chainId: string;
+};
 
-try {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ApiUrl>) {
+  try {
+    let { chainId: chainIdQuery } = req.query;
 
-  let {
-    chainId
-  } = req.query;
+    if (!chainIdQuery) {
+      const defaultChain = process.env.NEXT_PUBLIC_DEFAULT_CHAIN_ID;
 
-  if (!chainId) {
-    const defaultChain = process.env.NEXT_PUBLIC_DEFAULT_CHAIN_ID
-
-    if (!defaultChain) {
+      if (!defaultChain) {
         console.error("API defaultRpcUrl error: chainId was not provided");
         res.status(400).json({ error: "chainId was not provided" });
         return;
       }
-      chainId = defaultChain
-  }
-    const providerApiUrl = getProviderApiUrlFromEnv(Number.parseInt(chainId));
-  if (!providerApiUrl) {
-    console.error(`API defaultRpcUrl error: API URL for chain id ${chainId} was not set`);
-    res.status(500).json({ error: `API defaultRpcUrl error: API URL for chain id ${chainId} was not set` });
-    return;
-  }
-  const result = {
-    url: providerApiUrl
-  }
-  res.status(200).json(result);
-} catch (error) {
-  console.error(error);
-  res.status(500).json({ error: "An error occurred during blockchain method call" });
-}
+      chainIdQuery = defaultChain;
+    }
+    const chainId = typeof chainIdQuery === "string" ? Number.parseInt(chainIdQuery) : 0;
 
+    const providerApiUrl = getProviderApiUrlFromEnv(chainId);
+    if (!providerApiUrl) {
+      console.error(`API defaultRpcUrl error: API URL for chain id ${chainId} was not set`);
+      res.status(500).json({ error: `API defaultRpcUrl error: API URL for chain id ${chainId} was not set` });
+      return;
+    }
+    const result = {
+      url: providerApiUrl,
+    };
+    res.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred during blockchain method call" });
+  }
 }
