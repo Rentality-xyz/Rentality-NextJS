b9de69c2754800baefd73ab23712958d1f701a03
Protasov <hlib@digisol.agency>
2025-02-03 13:37:07 +0200

    update naming

--- src/components/investment/investCar.tsx
+++ src/components/investment/investCar.tsx
@@ -2,9 +2,8 @@ import RntButton from "../common/rntButton";
 import { useMemo, useState } from "react";
 import { displayMoneyWith2Digits } from "@/utils/numericFormatters";
 import RntInput from "../common/rntInput";
-import { InvestmentWithMetadata } from "@/model/blockchain/schemas";
 import { ENGINE_TYPE_PETROL_STRING, getEngineTypeString } from "@/model/EngineType";
-import { MappedInvestmentInfo } from "@/model/InvestmentInfo";
+import { InvestmentInfoWithMetadata } from "@/model/InvestmentInfo";
 
 type TFunction = (key: string, options?: { [key: string]: any }) => string;
 
@@ -19,7 +18,7 @@ export default function InvestCar({
   handleStartHosting,
   handleClaimIncome,
 }: {
-  searchInfo: MappedInvestmentInfo;
+  searchInfo: InvestmentInfoWithMetadata;
   handleInvest: (amount: number, investId: number) => void;
   disableButton: boolean;
   isSelected: boolean;
