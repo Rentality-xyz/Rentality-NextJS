dfda14a55fb673c4b0a2424263d606c668930bcb
Dima Denisenko <dendima80@gmail.com>
2025-02-12 15:00:38 +0200

    jira 458 to invest

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
 import Search from "./guest/search";
+import useUserRole from "@/hooks/useUserRole";
+import Loading from "@/components/common/Loading";
+import { useAuth } from "@/contexts/auth/authContext";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 
 function Home() {
-  return <Search />;
+  const { isLoading, userRole } = useUserRole();
+  const { isAuthenticated } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoading && userRole === "Host") {
+      router.push("/host");
+    }
+  }, [isLoading, userRole]);
+
+  if (!isAuthenticated) {
+    return <Search />;
+  }
+
+  return <Loading />;
 }
 
 Home.allowAnonymousAccess = Search.allowAnonymousAccess;
