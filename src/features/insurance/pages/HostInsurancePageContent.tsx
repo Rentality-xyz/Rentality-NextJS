b6ee3e8c56fed086a786402aa5949bc393f69e98
Artem Islamov <artem.islamov@eigenmethod.com>
2025-02-13 14:45:49 +0000

    extract useFetchInsurances and use react query

--- src/features/insurance/pages/HostInsurancePageContent.tsx
+++ src/features/insurance/pages/HostInsurancePageContent.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import PageTitle from "@/components/pageTitle/pageTitle";
 import CheckingLoadingAuth from "@/components/common/CheckingLoadingAuth";
@@ -7,32 +7,24 @@ import PaginationWrapper from "@/components/common/PaginationWrapper";
 import AddHostInsurance from "@/features/insurance/components/AddHostInsurance";
 import HostInsuranceFilters from "@/features/insurance/components/HostInsuranceFilters";
 import HostInsuranceTable from "@/features/insurance/components/HostInsuranceTable";
-import useInsurances, { InsuranceFiltersType } from "@/features/insurance/hooks/useInsurances";
+import useFetchInsurances, { InsuranceFiltersType } from "../hooks/useFetchInsurances";
 
 const defaultFilters: InsuranceFiltersType = {};
 
 export default function HostInsurancePageContent() {
   const itemsPerPage = 10;
   const [filters, setFilters] = useState<InsuranceFiltersType>(defaultFilters);
-  const { isLoading, data, fetchData } = useInsurances(true);
+  const { isLoading, data, fetchData } = useFetchInsurances(true, 1, itemsPerPage);
   const { t } = useTranslation();
 
   async function handleApplyFilters(filters: InsuranceFiltersType) {
     setFilters(filters);
-    await fetchData(filters, 1, itemsPerPage);
+    await fetchData(1, itemsPerPage, filters);
   }
 
   async function fetchDataForPage(page: number) {
-    await fetchData(filters, page, itemsPerPage);
+    await fetchData(page, itemsPerPage, filters);
   }
-
-  async function handleNewInsuranceAdded() {
-    await fetchData(filters, 1, itemsPerPage, true);
-  }
-
-  useEffect(() => {
-    fetchData(defaultFilters, 1, itemsPerPage);
-  }, [fetchData]);
 
   return (
     <>
@@ -48,7 +40,7 @@ export default function HostInsurancePageContent() {
               totalPages={data.totalPageCount}
               selectPage={fetchDataForPage}
             >
-              <HostInsuranceTable isLoading={isLoading} data={data.data} />
+              <HostInsuranceTable isLoading={isLoading} data={data.pageData} />
             </PaginationWrapper>
           </div>
         </RntSuspense>
