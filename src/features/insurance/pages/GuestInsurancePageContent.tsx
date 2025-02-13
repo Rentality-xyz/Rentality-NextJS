acb3b4f142bd9532ea9407143c49a6a657a1d9fa
Artem Islamov <artem.islamov@eigenmethod.com>
2025-02-13 16:15:22 +0000

    Save trip insurance logic review (and use react query)

--- src/features/insurance/pages/GuestInsurancePageContent.tsx
+++ src/features/insurance/pages/GuestInsurancePageContent.tsx
@@ -31,7 +31,7 @@ export default function GuestInsurancePageContent() {
       <PageTitle title={t("insurance.page_title")} />
       <CheckingLoadingAuth>
         <RntSuspense isLoading={isLoading}>
-          <AddGuestInsurance onNewInsuranceAdded={handleNewInsuranceAdded} />
+          <AddGuestInsurance />
           <h2 className="mt-4">{t("insurance.insurance_list")}</h2>
           <GuestInsuranceFilters defaultFilters={defaultFilters} onApply={handleApplyFilters} />
           <div className="mt-5 flex flex-col gap-4 rounded-2xl bg-rentality-bg p-4 pb-8">
--- src/features/insurance/pages/HostInsurancePageContent.tsx
+++ src/features/insurance/pages/HostInsurancePageContent.tsx
@@ -31,7 +31,7 @@ export default function HostInsurancePageContent() {
       <PageTitle title={t("insurance.page_title")} />
       <CheckingLoadingAuth>
         <RntSuspense isLoading={isLoading}>
-          <AddHostInsurance onNewInsuranceAdded={handleNewInsuranceAdded} />
+          <AddHostInsurance />
           <h2 className="mt-4">{t("insurance.insurance_list")}</h2>
           <HostInsuranceFilters defaultFilters={defaultFilters} onApply={handleApplyFilters} />
           <div className="mt-5 flex flex-col gap-4 rounded-2xl bg-rentality-bg p-4 pb-8">
