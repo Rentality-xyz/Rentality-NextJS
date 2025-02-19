3d62b1d920bbdf31caa2fdb33946cbec68deb5ad
Dima Denisenko <dendima80@gmail.com>
2025-02-19 16:56:38 +0200

    jira 939 cropper format photo listing

--- src/components/host/listingItem.tsx
+++ src/components/host/listingItem.tsx
@@ -37,7 +37,7 @@ export default function ListingItem({
     <div className="rnt-card flex flex-col overflow-hidden rounded-xl bg-rentality-bg">
       <div
         style={{ backgroundImage: `url(${carInfo.image})` }}
-        className="relative h-[212px] w-full flex-shrink-0 bg-cover bg-center"
+        className="relative h-[212px] w-full flex-shrink-0 bg-cover bg-center min-[1660px]:h-[176px] fullHD:h-[212px]"
       >
         <div className={statusClassName}>
           <strong>{`${getListingStatusTextFromStatus(carInfo.currentlyListed, t)}`}</strong>
