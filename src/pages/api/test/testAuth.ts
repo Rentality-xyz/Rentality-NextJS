fe27a9b4d03607e1bde28cc250b8765e4f8cf46b
Artem Islamov <artem.islamov@eigenmethod.com>
2025-02-19 14:00:38 +0000

    Add server auth logic with jose lib for verification

---
+++ src/pages/api/test/testAuth.ts
@@ -0,0 +1,20 @@
+import { checkRentalityAuthToken } from "@/features/auth/utils/serverAuth";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const authResult = await checkRentalityAuthToken(req);
+
+  if (!authResult.ok) {
+    console.error("testAuth error: ", authResult.error.message);
+    res.status(authResult.error.statusCode).json({
+      error:
+        authResult.error.statusCode !== 500
+          ? authResult.error.message
+          : "Something went wrong! Please wait a few minutes and try again",
+    });
+    return;
+  }
+
+  res.status(200).json({ ok: true });
+  return;
+}
