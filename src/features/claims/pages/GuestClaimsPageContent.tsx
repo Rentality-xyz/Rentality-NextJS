af5e89e10ce59340ca68bf04157f551c00eeaf32
Artem Islamov <artem.islamov@eigenmethod.com>
2025-02-21 16:47:34 +0000

    move create claim hooks inside  CreateClaim component

--- src/features/claims/pages/GuestClaimsPageContent.tsx
+++ src/features/claims/pages/GuestClaimsPageContent.tsx
@@ -3,57 +3,15 @@ import PageTitle from "@/components/pageTitle/pageTitle";
 import { useRntSnackbars } from "@/contexts/rntDialogsContext";
 import { useTranslation } from "react-i18next";
 import CreateClaim from "@/features/claims/components/CreateClaim";
-import { CreateClaimRequest } from "@/features/claims/models/CreateClaimRequest";
-import { Err, Result, TransactionErrorCode } from "@/model/utils/result";
 import RntSuspense from "@/components/common/rntSuspense";
 import useFetchClaims from "../hooks/useFetchClaims";
 import useUpdateClaim from "../hooks/useUpdateClaim";
-import useCreateClaim from "../hooks/useCreateClaim";
 
 function GuestClaimsPageContent() {
   const { t } = useTranslation();
   const { showInfo, showError, hideSnackbars } = useRntSnackbars();
   const { isLoading, data: claims, refetch } = useFetchClaims(false);
-  const { isLoading: isTripsLoading, tripInfos, createClaim } = useCreateClaim(false);
   const { payClaim, cancelClaim } = useUpdateClaim();
-
-  async function handleCreateClaim(
-    createClaimRequest: CreateClaimRequest
-  ): Promise<Result<boolean, TransactionErrorCode>> {
-    if (!createClaimRequest.tripId) {
-      showError(t("claims.host.select_trip"));
-      return Err("ERROR");
-    }
-    if (!createClaimRequest.claimType && createClaimRequest.claimType !== BigInt(0)) {
-      showError(t("claims.host.select_type"));
-      return Err("ERROR");
-    }
-    if (!createClaimRequest.description) {
-      showError(t("claims.host.enter_description"));
-      return Err("ERROR");
-    }
-    if (!createClaimRequest.amountInUsdCents) {
-      showError(t("claims.host.enter_amount"));
-      return Err("ERROR");
-    }
-
-    showInfo(t("common.info.sign"));
-
-    const result = await createClaim(createClaimRequest);
-
-    hideSnackbars();
-
-    if (result.ok) {
-      refetch();
-    } else {
-      if (result.error === "NOT_ENOUGH_FUNDS") {
-        showError(t("common.add_fund_to_wallet"));
-      } else {
-        showError(t("claims.host.claim_failed"));
-      }
-    }
-    return result;
-  }
 
   async function handleCancelClaim(claimId: number) {
     showInfo(t("common.info.sign"));
@@ -94,8 +52,8 @@ function GuestClaimsPageContent() {
   return (
     <>
       <PageTitle title={t("claims.title")} />
-      <RntSuspense isLoading={isLoading || isTripsLoading}>
-        <CreateClaim createClaim={handleCreateClaim} tripInfos={tripInfos} isHost={false} />
+      <RntSuspense isLoading={isLoading}>
+        <CreateClaim onClaimAdded={refetch} />
         <ClaimHistory
           claims={claims}
           payClaim={handlePayClaim}
--- src/features/claims/pages/HostClaimsPageContent.tsx
+++ src/features/claims/pages/HostClaimsPageContent.tsx
@@ -2,58 +2,16 @@ import ClaimHistory from "@/features/claims/components/ClaimHistory";
 import CreateClaim from "@/features/claims/components/CreateClaim";
 import PageTitle from "@/components/pageTitle/pageTitle";
 import { useRntSnackbars } from "@/contexts/rntDialogsContext";
-import { CreateClaimRequest } from "@/features/claims/models/CreateClaimRequest";
 import { useTranslation } from "react-i18next";
-import { Err, Result, TransactionErrorCode } from "@/model/utils/result";
 import RntSuspense from "@/components/common/rntSuspense";
 import useFetchClaims from "../hooks/useFetchClaims";
 import useUpdateClaim from "../hooks/useUpdateClaim";
-import useCreateClaim from "../hooks/useCreateClaim";
 
 function HostClaimsPageContent() {
   const { showInfo, showError, hideSnackbars } = useRntSnackbars();
   const { isLoading, data: claims, refetch } = useFetchClaims(true);
-  const { isLoading: isTripsLoading, tripInfos, createClaim } = useCreateClaim(true);
   const { payClaim, cancelClaim } = useUpdateClaim();
   const { t } = useTranslation();
-
-  async function handleCreateClaim(
-    createClaimRequest: CreateClaimRequest
-  ): Promise<Result<boolean, TransactionErrorCode>> {
-    if (!createClaimRequest.tripId) {
-      showError(t("claims.host.select_trip"));
-      return Err("ERROR");
-    }
-    if (!createClaimRequest.claimType && createClaimRequest.claimType !== BigInt(0)) {
-      showError(t("claims.host.select_type"));
-      return Err("ERROR");
-    }
-    if (!createClaimRequest.description) {
-      showError(t("claims.host.enter_description"));
-      return Err("ERROR");
-    }
-    if (!createClaimRequest.amountInUsdCents) {
-      showError(t("claims.host.enter_amount"));
-      return Err("ERROR");
-    }
-
-    showInfo(t("common.info.sign"));
-
-    const result = await createClaim(createClaimRequest);
-
-    hideSnackbars();
-
-    if (result.ok) {
-      refetch();
-    } else {
-      if (result.error === "NOT_ENOUGH_FUNDS") {
-        showError(t("common.add_fund_to_wallet"));
-      } else {
-        showError(t("claims.host.claim_failed"));
-      }
-    }
-    return result;
-  }
 
   async function handleCancelClaim(claimId: number) {
     showInfo(t("common.info.sign"));
@@ -94,8 +52,8 @@ function HostClaimsPageContent() {
   return (
     <>
       <PageTitle title={t("claims.title")} />
-      <RntSuspense isLoading={isLoading || isTripsLoading}>
-        <CreateClaim createClaim={handleCreateClaim} tripInfos={tripInfos} isHost={true} />
+      <RntSuspense isLoading={isLoading}>
+        <CreateClaim onClaimAdded={refetch} />
         <ClaimHistory
           claims={claims}
           payClaim={handlePayClaim}
