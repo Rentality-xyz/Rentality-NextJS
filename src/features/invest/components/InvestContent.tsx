a872a41f095fd91ddf64069700cbc852443e8351
Dima Denisenko <dendima80@gmail.com>
2025-02-24 12:42:47 +0200

    jira 939 cropper bag fix

--- src/features/invest/components/InvestContent.tsx
+++ src/features/invest/components/InvestContent.tsx
@@ -86,7 +86,7 @@ export default function InvestContent({ isHost }: InvestContentProps) {
       )}
 
       <RntFilterSelect
-        className="border-gradient w-60 justify-center border-0 bg-transparent text-lg text-rentality-secondary"
+        className="btn_input_border-gradient w-60 justify-center bg-transparent text-lg text-rentality-secondary"
         id="invest_filter"
         value={filterInvestBy ? filterInvest[filterInvestBy] : Object.values(filterInvest ?? {})[0]}
         onChange={(e) => {
