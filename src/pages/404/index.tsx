71ebe81056b5869222615de0f892354db114a64f
Artem Islamov <artem.islamov@eigenmethod.com>
2025-02-24 17:39:30 +0000

    move image to public folder, set allowAnonymousAccess, clean up code

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,26 +1,22 @@
-import Image from "next/image";
-import carLoading from "@/images/car_loading.png";
-import logoBaseWhite from "@/images/logo_base_white.png";
-import RntButton from "@/components/common/rntButton";
-import Link from "next/link";
-import carLoadingDesktop from "@/images/car_loading_desktop.png";
-import carLoadingDesktopLong from "@/images/car_404.png";
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 import { useTranslation } from "react-i18next";
 
 function Rentality404() {
-
   const { t } = useTranslation();
 
   return (
-      <div className="flex w-full h-full flex-col font-['Montserrat',Arial,sans-serif] gap-6 items-center justify-center" >
-        <p className="text-xl">{`404 | ${t("common.info.page_not_found")}`}</p>
-        <Link className="text-rentality-secondary" href="/guest/search" target="_self">
-          <span>{t("common.info.back_to_car_search")}</span>
-        </Link>
-        <Image src={carLoadingDesktopLong} alt="" className="w-[600px] hidden sm:block" />
-      </div>
+    <div className="flex h-full w-full flex-col items-center justify-center gap-6 font-['Montserrat',Arial,sans-serif]">
+      <p className="text-xl">{`404 | ${t("common.info.page_not_found")}`}</p>
+      <Link className="text-rentality-secondary" href="/guest/search" target="_self">
+        <span>{t("common.info.back_to_car_search")}</span>
+      </Link>
+      <Image src="/images/car_404.png" alt="" className="hidden w-[600px] sm:block" width={1000} height={1000} />
+    </div>
   );
 }
+
+Rentality404.allowAnonymousAccess = true;
 
 export default Rentality404;
--- src/pages/technical_work/index.tsx
+++ src/pages/technical_work/index.tsx
@@ -1,20 +1,19 @@
-import { useTranslation } from "react-i18next";
-import Link from "next/link";
-import Image from "next/image";
-import carLoadingDesktop from "@/images/car_404.png";
 import React from "react";
+import Image from "next/image";
+import { useTranslation } from "react-i18next";
 
 function TechnicalWork() {
-
   const { t } = useTranslation();
 
   return (
-      <div className="flex w-screen h-screen flex-col font-['Montserrat',Arial,sans-serif] content-center justify-center items-center gap-6" >
-        <p className="text-2xl">{t("common.info.temporarily_unavailable")}</p>
-        <p className="text-rentality-secondary text-center">{t("common.info.new_functionality_deploy")}</p>
-        <Image src={carLoadingDesktop} alt="" className="w-[600px] hidden sm:block" />
-      </div>
+    <div className="flex h-screen w-screen flex-col content-center items-center justify-center gap-6 font-['Montserrat',Arial,sans-serif]">
+      <p className="text-2xl">{t("common.info.temporarily_unavailable")}</p>
+      <p className="text-center text-rentality-secondary">{t("common.info.new_functionality_deploy")}</p>
+      <Image src="/images/car_404.png" alt="" className="hidden w-[600px] sm:block" width={1000} height={1000} />
+    </div>
   );
 }
+
+TechnicalWork.allowAnonymousAccess = true;
 
 export default TechnicalWork;
