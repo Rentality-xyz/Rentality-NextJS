1b159ec9543fdee4abaf83f79e2df248f8d6b7c6
Dima Denisenko <dendima80@gmail.com>
2025-03-10 14:45:03 +0200

    jira 1007 bag fix

--- src/components/tripCard/tripAdditionalActions.tsx
+++ src/components/tripCard/tripAdditionalActions.tsx
@@ -173,10 +173,10 @@ function TripAdditionalActions({
             setConfirmParams={setConfirmParams}
           />
         )}
-        <div className="flex w-1/2 flex-col gap-4 py-4">
+        <div className="flex w-full flex-col gap-4 py-4">
           {hasTripPhotosFeatureFlag &&
             (tripInfo.status == TripStatus.CheckedInByHost || tripInfo.status == TripStatus.CheckedOutByGuest) && (
-              <div className="flex w-full flex-col md:flex-1 lg:w-1/3 lg:flex-none">
+              <div className="flex w-full flex-col">
                 <CarPhotosUploadButton
                   ref={carPhotosUploadButtonRef}
                   isHost={isHost}
