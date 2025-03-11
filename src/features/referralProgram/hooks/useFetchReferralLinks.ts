1e7cb110722c3b3dd7f5da53afe23c3071192b28
Artem Islamov <artem.islamov@eigenmethod.com>
2025-03-11 11:20:14 +0000

    add ethereumInfo?.walletAddress to some useQuery keys

--- src/features/referralProgram/hooks/useFetchReferralLinks.ts
+++ src/features/referralProgram/hooks/useFetchReferralLinks.ts
@@ -1,4 +1,5 @@
 import { useRentality } from "@/contexts/rentalityContext";
+import { useEthereum } from "@/contexts/web3/ethereumContext";
 import { logger } from "@/utils/logger";
 import { ZERO_4_BYTES_HASH } from "@/utils/wallet";
 import { useQuery } from "@tanstack/react-query";
@@ -7,10 +8,11 @@ export const REFERRAL_LINKS_QUERY_KEY = "ReferralLinks";
 type QueryData = { inviteHash: string; usedInviteHash: string };
 
 const useFetchReferralLinks = () => {
+  const ethereumInfo = useEthereum();
   const { rentalityContracts } = useRentality();
 
   return useQuery<QueryData>({
-    queryKey: [REFERRAL_LINKS_QUERY_KEY],
+    queryKey: [REFERRAL_LINKS_QUERY_KEY, ethereumInfo?.walletAddress],
     initialData: { inviteHash: "", usedInviteHash: "" },
     queryFn: async () => {
       if (!rentalityContracts) {
@@ -28,7 +30,7 @@ const useFetchReferralLinks = () => {
         usedInviteHash: result.value.savedHash !== ZERO_4_BYTES_HASH ? result.value.savedHash : "",
       };
     },
-    enabled: !!rentalityContracts,
+    enabled: !!rentalityContracts && !!ethereumInfo,
   });
 };
 
