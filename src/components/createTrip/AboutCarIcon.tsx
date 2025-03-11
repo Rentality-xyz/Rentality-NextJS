2e65046e8274d931be6474d542c68c5cbc0f6a97
danyloday <danil.vladyka1@gmail.com>
2025-03-11 12:48:33 +0200

    RTY-1017: Fixed image size

--- src/components/createTrip/AboutCarIcon.tsx
+++ src/components/createTrip/AboutCarIcon.tsx
@@ -4,22 +4,18 @@ import { cn } from "@/utils";
 export function AboutCarIcon({
   className,
   image,
-  width,
-  height,
   title,
   text,
 }: {
   className?: string;
   image: string;
-  width?: number;
-  height?: number;
   title?: string;
   text?: string | number;
 }) {
   const cClassName = cn("flex items-center gap-1", className);
   return (
     <div className={cClassName}>
-      <Image className="me-1" src={image} width={width ?? 30} height={height ?? 30} alt="" />
+      <Image className="me-1" src={image} width={30} height={30} alt="" />
       <div className="flex flex-col">
         {title && <p>{title}:</p>}
         <p>{text}</p>
