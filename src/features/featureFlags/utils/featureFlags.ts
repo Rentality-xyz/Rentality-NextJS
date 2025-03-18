54026c193d2c8938f803bafff1443468ea96ecdf
Artem Islamov <artem.islamov@eigenmethod.com>
2025-03-18 13:06:56 +0000

    save car metadata to pinata logic review

--- src/features/featureFlags/utils/featureFlags.ts
+++ src/features/featureFlags/utils/featureFlags.ts
@@ -6,7 +6,7 @@ export async function hasFeatureFlag(name: string): Promise<boolean> {
   const response: AxiosResponse = await axios.get(`/api/featureFlags?name=${name}`);
 
   if (response.status !== 200) {
-    logger.info("Feature flag was not fetched: " + response.status + " with data " + response.data);
+    logger.error("Feature flag was not fetched: " + response.status + " with data " + response.data);
     return false;
   }
 
