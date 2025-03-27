4150184128153632c4c0d060ab505c50c96102ec
Dima Denisenko <dendima80@gmail.com>
2025-03-28 00:06:39 +0200

    jira 1035 fix bag

--- src/features/featureFlags/utils/featureFlags.ts
+++ src/features/featureFlags/utils/featureFlags.ts
@@ -3,14 +3,12 @@ import axios from "@/utils/cachedAxios";
 import { logger } from "@/utils/logger";
 
 export async function hasFeatureFlag(name: string): Promise<boolean> {
-  //TODO
-  // const response: AxiosResponse = await axios.get(`/api/featureFlags?name=${name}`);
-  //
-  // if (response.status !== 200) {
-  //   logger.error("Feature flag was not fetched: " + response.status + " with data " + response.data);
-  //   return false;
-  // }
-  //
-  // return response.data == "true";
-  return true;
+  const response: AxiosResponse = await axios.get(`/api/featureFlags?name=${name}`);
+
+  if (response.status !== 200) {
+    logger.error("Feature flag was not fetched: " + response.status + " with data " + response.data);
+    return false;
+  }
+
+  return response.data == "true";
 }
