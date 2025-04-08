3eeb8ec7e16a6c3f274e86d8686894344e0f31e0
Artem Islamov <artem.islamov@eigenmethod.com>
2025-04-08 16:03:46 +0100

    Get text from option for value

--- src/utils/react.ts
+++ src/utils/react.ts
@@ -1,5 +1,23 @@
+import React from "react";
+
 export function assertIsNode(event: EventTarget | null): asserts event is Node {
   if (!event || !("nodeType" in event)) {
     throw new Error(`Node expected`);
   }
+}
+
+export function extractTextFromReactNode(node: React.ReactNode): string {
+  if (typeof node === "string" || typeof node === "number") {
+    return String(node);
+  }
+
+  if (Array.isArray(node)) {
+    return node.map(extractTextFromReactNode).join("");
+  }
+
+  if (React.isValidElement(node)) {
+    return extractTextFromReactNode(node.props.children);
+  }
+
+  return "";
 }
