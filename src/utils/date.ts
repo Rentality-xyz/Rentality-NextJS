c925b70ca6e72a44c9775b0455da4cad7e5d49f1
Artem Islamov <artem.islamov@eigenmethod.com>
2025-04-28 10:27:29 +0100

    fix calculateDaysByBlockchainLogic

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -3,8 +3,8 @@ import { isEmpty } from "./string";
 import { UTC_TIME_ZONE_ID } from "./constants";
 
 export const calculateDaysByBlockchainLogic = (dateFrom: Date, dateTo: Date) => {
-  let difference = moment.utc(dateTo).unix() - moment.utc(dateFrom).unix();
-  let totalDays = Math.max(Math.ceil(difference / (24 * 60 * 60 * 1000)), 1);
+  let differenceDays = moment.utc(dateTo).diff(moment.utc(dateFrom), "days", true);
+  let totalDays = Math.max(Math.ceil(differenceDays), 1);
   return totalDays;
 };
 
