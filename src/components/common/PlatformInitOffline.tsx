145583da3b9ab9cd1848beffec44957b99741fdc
danyloday <danil.vladyka1@gmail.com>
2025-04-30 13:37:45 +0300

    Added no internet page

---
+++ src/components/common/PlatformInitOffline.tsx
@@ -0,0 +1,29 @@
+import React, { ReactElement } from "react";
+import Image from "next/image";
+import { useTranslation } from "react-i18next";
+import RntButton from "@/components/common/rntButton";
+import { useRouter } from "next/navigation";
+
+function PlatformInitOffline() {
+  const { t } = useTranslation();
+  const router = useRouter();
+
+  function handleTryAgainClick() {
+    router.refresh()
+  }
+
+  return (
+    <div className="flex h-screen w-full flex-col content-center items-center justify-center gap-6">
+      <p className="text-2xl font-bold">{t("common.info.internet_troubles")}</p>
+      <p className="text-lg">{t("common.info.check_internet")}</p>
+      <RntButton className="w-72" onClick={handleTryAgainClick}>
+        {t("common.info.try_again")}
+      </RntButton>
+      <Image src="/images/car_404.png" alt="" className="hidden w-[600px] sm:block" width={1000} height={1000} />
+    </div>
+  );
+}
+
+PlatformInitOffline.allowAnonymousAccess = true;
+
+export default PlatformInitOffline;
