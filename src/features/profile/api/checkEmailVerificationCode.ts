5d5f480a9ee1981e853eeb3cf33e4b4a39e55057
Artem Islamov <artem.islamov@eigenmethod.com>
2025-06-06 18:54:32 +0100

    update check email verification

--- src/features/profile/api/checkEmailVerificationCode.ts
+++ src/features/profile/api/checkEmailVerificationCode.ts
@@ -9,32 +9,32 @@ import { IRentalityGatewayContract } from "@/features/blockchain/models/IRentali
 import getProviderApiUrlFromEnv from "@/utils/api/providerApiUrl";
 import { logger } from "@/utils/logger";
 
-const CODE_EXPIRATION_TIME_MS = 5 * 60 * 1000;
+const CODE_EXPIRATION_TIME_MS = 1000 * 60 * 24;
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== "POST") {
-    logger.error("compareSmsVerificationCode error: method not allowed");
+export default async function checkEmailVerificationCodeHandler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "GET") {
+    logger.error("checkEmailVerificationCodeHandler error: method not allowed");
     res.status(405).json({ error: "Method not allowed" });
     return;
   }
 
-  const { userAddress, phoneNumber, enteredCode, smsHash, timestamp, chainId } = req.body;
+  const { userAddress, email, enteredCode, hash, timestamp, chainId } = req.query;
 
   if (
     !userAddress ||
     typeof userAddress !== "string" ||
-    !phoneNumber ||
-    typeof phoneNumber !== "string" ||
+    !email ||
+    typeof email !== "string" ||
     !enteredCode ||
     typeof enteredCode !== "string" ||
-    !smsHash ||
-    typeof smsHash !== "string" ||
+    !hash ||
+    typeof hash !== "string" ||
     !timestamp ||
     typeof timestamp !== "number" ||
     !chainId ||
     typeof chainId !== "number"
   ) {
-    logger.error("compareSmsVerificationCode error: invalid parameters");
+    logger.error("checkEmailVerificationCodeHandler error: invalid parameters");
     res.status(400).json({ error: "Invalid parameters" });
     return;
   }
@@ -42,27 +42,27 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const now = Date.now();
 
   if (now - timestamp > CODE_EXPIRATION_TIME_MS) {
-    logger.error("compareSmsVerificationCode error: verification code expired");
+    logger.error("checkEmailVerificationCodeHandler error: verification code expired");
     res.status(400).json({ error: "Verification code expired" });
     return;
   }
 
   const enteredCodeHash = crypto
     .createHmac("sha256", env.VERIFICATION_HMAC_SHA256_SECRET_KEY)
-    .update(phoneNumber + enteredCode + timestamp)
+    .update(userAddress + email + enteredCode + timestamp + chainId)
     .digest("hex");
 
-  const isVerified = smsHash === enteredCodeHash;
+  const isVerified = hash === enteredCodeHash;
 
   if (!isVerified) {
-    logger.info(`Phone number didn't verified`);
+    logger.info(`Email didn't verified`);
     res.status(200).json({ isVerified: false });
     return;
   }
 
   const MANAGER_PRIVATE_KEY = env.MANAGER_PRIVATE_KEY;
   if (isEmpty(MANAGER_PRIVATE_KEY)) {
-    logger.error("compareSmsVerificationCode error: private key was not set");
+    logger.error("checkEmailVerificationCodeHandler error: private key was not set");
     res.status(500).json({ error: "Manager private key was not set" });
     return;
   }
@@ -70,32 +70,34 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const providerApiUrl = getProviderApiUrlFromEnv(chainId);
 
   if (!providerApiUrl) {
-    logger.error(`compareSmsVerificationCode error: API URL for chain id ${chainId} was not set`);
-    res.status(500).json({ error: `compareSmsVerificationCode error: API URL for chain id ${chainId} was not set` });
+    logger.error(`checkEmailVerificationCodeHandler error: API URL for chain id ${chainId} was not set`);
+    res
+      .status(500)
+      .json({ error: `checkEmailVerificationCodeHandler error: API URL for chain id ${chainId} was not set` });
     return;
   }
 
   const saveVerificationStatusResult = await saveVerificationSuccessStatus(
     userAddress,
-    phoneNumber,
+    email,
     MANAGER_PRIVATE_KEY,
     providerApiUrl
   );
 
   if (!saveVerificationStatusResult.ok) {
-    logger.error(`compareSmsVerificationCode error: ${saveVerificationStatusResult.error}`);
+    logger.error(`checkEmailVerificationCodeHandler error: ${saveVerificationStatusResult.error}`);
     res.status(500).json({ error: saveVerificationStatusResult.error });
     return;
   }
 
-  logger.info(`Phone number verified successfully`);
+  logger.info(`Email verified successfully`);
   res.status(200).json({ isVerified: true });
   return;
 }
 
 async function saveVerificationSuccessStatus(
   userAddress: string,
-  phoneNumber: string,
+  email: string,
   managerPrivateKey: string,
   providerApiUrl: string
 ): Promise<Result<boolean, string>> {
@@ -115,7 +117,7 @@ async function saveVerificationSuccessStatus(
   }
 
   try {
-    const transaction = await rentality.setPhoneNumber(userAddress, phoneNumber, true);
+    const transaction = await rentality.setEmail(userAddress, email, true);
     await transaction.wait();
     return Ok(true);
   } catch (error) {
