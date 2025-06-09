24df4ae2953879763a2bf6c752d90b6c22d02172
Artem Islamov <artem.islamov@eigenmethod.com>
2025-06-09 12:46:08 +0100

    update email verifacation logic

--- src/features/profile/api/checkEmailVerificationCode.ts
+++ src/features/profile/api/checkEmailVerificationCode.ts
@@ -1,5 +1,4 @@
 import { env } from "@/utils/env";
-import crypto from "crypto";
 import { NextApiRequest, NextApiResponse } from "next";
 import { Err, Ok, Result } from "@/model/utils/result";
 import { isEmpty } from "@/utils/string";
@@ -8,8 +7,18 @@ import { getEtherContractWithSigner } from "@/abis";
 import { IRentalityGatewayContract } from "@/features/blockchain/models/IRentalityGateway";
 import getProviderApiUrlFromEnv from "@/utils/api/providerApiUrl";
 import { logger } from "@/utils/logger";
+import { getEthersContractProxy } from "@/features/blockchain/models/EthersContractProxy";
+import { getEmailVerificationHash } from "../utils/email";
 
 const CODE_EXPIRATION_TIME_MS = 1000 * 60 * 24;
+type CheckEmailVerificationCodeParams = {
+  walletAddress: string;
+  email: string;
+  verificationCode: string;
+  timestamp: number;
+  chainId: number;
+  hash: string;
+};
 
 export default async function checkEmailVerificationCodeHandler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "GET") {
@@ -18,72 +27,30 @@ export default async function checkEmailVerificationCodeHandler(req: NextApiRequ
     return;
   }
 
-  const { userAddress, email, enteredCode, hash, timestamp, chainId } = req.query;
-
-  if (
-    !userAddress ||
-    typeof userAddress !== "string" ||
-    !email ||
-    typeof email !== "string" ||
-    !enteredCode ||
-    typeof enteredCode !== "string" ||
-    !hash ||
-    typeof hash !== "string" ||
-    !timestamp ||
-    typeof timestamp !== "number" ||
-    !chainId ||
-    typeof chainId !== "number"
-  ) {
-    logger.error("checkEmailVerificationCodeHandler error: invalid parameters");
-    res.status(400).json({ error: "Invalid parameters" });
+  const requestResult = validateRequest(req);
+  if (!requestResult.ok) {
+    logger.error(`checkEmailVerificationCodeHandler validation error: ${requestResult.error.message}`);
+    res.status(400).json({ error: "Validation error: " + requestResult.error.message });
     return;
   }
 
-  const now = Date.now();
+  const { walletAddress, email, chainId } = requestResult.value;
 
-  if (now - timestamp > CODE_EXPIRATION_TIME_MS) {
-    logger.error("checkEmailVerificationCodeHandler error: verification code expired");
-    res.status(400).json({ error: "Verification code expired" });
+  const envsResult = validateEnvs(chainId);
+  if (!envsResult.ok) {
+    logger.error(`checkEmailVerificationCodeHandler env validation error: ${envsResult.error.message}`);
+    res.status(500).json({ error: "Something went wrong! Please wait a few minutes and try again" });
     return;
   }
 
-  const enteredCodeHash = crypto
-    .createHmac("sha256", env.VERIFICATION_HMAC_SHA256_SECRET_KEY)
-    .update(userAddress + email + enteredCode + timestamp + chainId)
-    .digest("hex");
-
-  const isVerified = hash === enteredCodeHash;
-
-  if (!isVerified) {
-    logger.info(`Email didn't verified`);
-    res.status(200).json({ isVerified: false });
+  const validateVerificationCodeResult = validateVerificationCode(requestResult.value);
+  if (!validateVerificationCodeResult.ok) {
+    logger.error(`checkEmailVerificationCodeHandler validation error: ${validateVerificationCodeResult.error.message}`);
+    res.status(400).json({ error: "Validation error: " + validateVerificationCodeResult.error.message });
     return;
   }
 
-  const MANAGER_PRIVATE_KEY = env.MANAGER_PRIVATE_KEY;
-  if (isEmpty(MANAGER_PRIVATE_KEY)) {
-    logger.error("checkEmailVerificationCodeHandler error: private key was not set");
-    res.status(500).json({ error: "Manager private key was not set" });
-    return;
-  }
-
-  const providerApiUrl = getProviderApiUrlFromEnv(chainId);
-
-  if (!providerApiUrl) {
-    logger.error(`checkEmailVerificationCodeHandler error: API URL for chain id ${chainId} was not set`);
-    res
-      .status(500)
-      .json({ error: `checkEmailVerificationCodeHandler error: API URL for chain id ${chainId} was not set` });
-    return;
-  }
-
-  const saveVerificationStatusResult = await saveVerificationSuccessStatus(
-    userAddress,
-    email,
-    MANAGER_PRIVATE_KEY,
-    providerApiUrl
-  );
-
+  const saveVerificationStatusResult = await saveVerificationSuccessStatus(walletAddress, chainId, email);
   if (!saveVerificationStatusResult.ok) {
     logger.error(`checkEmailVerificationCodeHandler error: ${saveVerificationStatusResult.error}`);
     res.status(500).json({ error: saveVerificationStatusResult.error });
@@ -91,37 +58,113 @@ export default async function checkEmailVerificationCodeHandler(req: NextApiRequ
   }
 
   logger.info(`Email verified successfully`);
-  res.status(200).json({ isVerified: true });
+  res.status(200).json({ success: true });
   return;
 }
 
+function validateRequest(req: NextApiRequest): Result<CheckEmailVerificationCodeParams> {
+  const {
+    walletAddress,
+    email,
+    hash,
+    timestamp: timestampString,
+    chainId: chainIdString,
+    verificationCode,
+  } = req.query;
+
+  if (!walletAddress || typeof walletAddress !== "string") {
+    return Err(new Error("walletAddress is missing or not a string"));
+  }
+  if (!email || typeof email !== "string") {
+    return Err(new Error("email is missing or not a string"));
+  }
+  if (!hash || typeof hash !== "string") {
+    return Err(new Error("hash is missing or not a string"));
+  }
+  if (chainIdString == undefined || isNaN(Number(chainIdString))) {
+    return Err(new Error("chainId is missing or not a number"));
+  }
+  if (timestampString == undefined || isNaN(Number(timestampString))) {
+    return Err(new Error("timestamp is missing or not a number"));
+  }
+  if (!verificationCode || typeof verificationCode !== "string") {
+    return Err(new Error("verificationCode is missing or not a string"));
+  }
+
+  const chainId = Number(chainIdString);
+  const timestamp = Number(timestampString);
+
+  return Ok({ walletAddress, email, verificationCode, timestamp, chainId, hash });
+}
+
+function validateEnvs(chainId: number): Result<boolean> {
+  const VERIFICATION_HMAC_SHA256_SECRET_KEY = env.VERIFICATION_HMAC_SHA256_SECRET_KEY;
+  if (isEmpty(VERIFICATION_HMAC_SHA256_SECRET_KEY)) {
+    return Err(new Error("VERIFICATION_HMAC_SHA256_SECRET_KEY was not set"));
+  }
+
+  const privateKey = env.MANAGER_PRIVATE_KEY;
+  if (isEmpty(privateKey)) {
+    return Err(new Error("MANAGER_PRIVATE_KEY was not set"));
+  }
+
+  const providerApiUrl = getProviderApiUrlFromEnv(chainId);
+  if (isEmpty(providerApiUrl)) {
+    return Err(new Error(`API URL for chain id ${chainId} was not set`));
+  }
+
+  return Ok(true);
+}
+
+function validateVerificationCode(params: CheckEmailVerificationCodeParams): Result<boolean> {
+  const now = Date.now();
+
+  if (now - params.timestamp > CODE_EXPIRATION_TIME_MS) {
+    return Err(new Error("verification code expired"));
+  }
+
+  const hashToVerily = getEmailVerificationHash(
+    env.VERIFICATION_HMAC_SHA256_SECRET_KEY,
+    params.walletAddress,
+    params.email,
+    params.verificationCode,
+    params.timestamp,
+    params.chainId
+  );
+
+  if (params.hash !== hashToVerily) {
+    return Err(new Error("hash is not valid"));
+  }
+
+  return Ok(true);
+}
+
 async function saveVerificationSuccessStatus(
-  userAddress: string,
-  email: string,
-  managerPrivateKey: string,
-  providerApiUrl: string
-): Promise<Result<boolean, string>> {
-  if (!userAddress || isEmpty(userAddress)) {
-    logger.error("User address is empty");
-    return Err("User address  is empty");
+  walletAddress: string,
+  chainId: number,
+  email: string
+): Promise<Result<boolean>> {
+  const privateKey = env.MANAGER_PRIVATE_KEY;
+  if (isEmpty(privateKey)) {
+    return Err(new Error("MANAGER_PRIVATE_KEY was not set"));
+  }
+
+  const providerApiUrl = getProviderApiUrlFromEnv(chainId);
+  if (isEmpty(providerApiUrl)) {
+    return Err(new Error(`API URL for chain id ${chainId} was not set`));
   }
 
   const provider = new JsonRpcProvider(providerApiUrl);
-  const wallet = new Wallet(managerPrivateKey, provider);
-
-  const rentality = (await getEtherContractWithSigner("gateway", wallet)) as unknown as IRentalityGatewayContract;
-
-  if (rentality === null) {
-    logger.error("rentality is null");
-    return Err("rentality is null");
+  const wallet = new Wallet(privateKey, provider);
+  const rentalityGateway = (await getEtherContractWithSigner(
+    "gateway",
+    wallet
+  )) as unknown as IRentalityGatewayContract;
+  if (!rentalityGateway) {
+    return Err(new Error("rentalityGateway is null"));
   }
 
-  try {
-    const transaction = await rentality.setEmail(userAddress, email, true);
-    await transaction.wait();
-    return Ok(true);
-  } catch (error) {
-    logger.error("saveVerificationSuccessStatus error", error);
-    return Err(`saveVerificationSuccessStatus error ${error}`);
-  }
+  const gatewayProxy = getEthersContractProxy(rentalityGateway);
+
+  return gatewayProxy.setEmail(walletAddress, email, true);
 }
--- src/pages/api/profile/sendEmailVerificationCode.ts
+++ src/pages/api/profile/sendEmailVerificationCode.ts
@@ -2,5 +2,10 @@ import { NextApiRequest, NextApiResponse } from "next";
 import sendEmailVerificationCodeHandler from "@/features/profile/api/sendEmailVerificationCode";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  return await sendEmailVerificationCodeHandler(req, res);
+  const protocol = req.headers["x-forwarded-proto"] || "http";
+  const host = req.headers.host;
+  const baseUrl = `${protocol}://${host}`;
+  const checkUrlBase = `${baseUrl}/api/checkEmailVerificationCode`;
+
+  return await sendEmailVerificationCodeHandler(req, res, checkUrlBase);
 }
