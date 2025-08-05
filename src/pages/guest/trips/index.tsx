98d0da53da2d55d40f3e329efcc0d0a953e1fbb4
danyloday <danil.vladyka1@gmail.com>
2025-08-05 12:14:21 +0300

    RTY-1100: Added redirect

--- src/pages/guest/trips/index.tsx
+++ src/pages/guest/trips/index.tsx
@@ -1,6 +1,18 @@
 import Booked from "./booked";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 function Trips() {
+
+  const router = useRouter();
+  const bookedPath = "/guest/trips/booked"
+
+  useEffect(() => {
+    if (router.pathname !== bookedPath) {
+      router.push(bookedPath);
+    }
+  }, [router]);
+
   return <Booked />;
 }
 
