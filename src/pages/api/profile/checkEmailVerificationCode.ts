8a507f0bb769a0ec98dff472337b354dbd256c66
danyloday <danil.vladyka1@gmail.com>
2025-08-08 11:33:57 +0300

    RTY-1100,243: Fixed url

--- src/pages/api/profile/checkEmailVerificationCode.ts
+++ src/pages/api/profile/checkEmailVerificationCode.ts
@@ -1,10 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import checkEmailVerificationCodeHandler from "@/features/profile/api/checkEmailVerificationCode";
+import { env } from "process";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const protocol = req.headers["x-forwarded-proto"] || "http";
   const host = req.headers.host;
-  const baseUrl = `${protocol}://${host}`;
+
+  const fallbackBaseUrl = `${protocol}://${host}`;
+  const envBaseUrl = env.BASE_URL;
+
+  const baseUrl = envBaseUrl && envBaseUrl.trim() !== ""
+    ? envBaseUrl
+    : fallbackBaseUrl;
 
   return await checkEmailVerificationCodeHandler(req, res, baseUrl);
 }
--- src/pages/api/scheduler/processBlockchainEvents.ts
+++ src/pages/api/scheduler/processBlockchainEvents.ts
@@ -1,10 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import processBlockchainEventsHandler from "@/features/scheduler/eventProcessing/api/processBlockchainEvents";
+import { env } from "process";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const protocol = req.headers["x-forwarded-proto"] || "http";
   const host = req.headers.host;
-  const baseUrl = `${protocol}://${host}`;
+
+  const fallbackBaseUrl = `${protocol}://${host}`;
+  const envBaseUrl = env.BASE_URL;
+
+  const baseUrl = envBaseUrl && envBaseUrl.trim() !== ""
+    ? envBaseUrl
+    : fallbackBaseUrl;
 
   return await processBlockchainEventsHandler(req, res, baseUrl);
 }
