e5113c3732c5e417d6734744ae014546b4eb5291
danyloday <danil.vladyka1@gmail.com>
2025-10-31 12:30:48 +0200

    RTY-1094: tutorial video

--- src/components/RnD/AddFunds.tsx
+++ src/components/RnD/AddFunds.tsx
@@ -3,12 +3,13 @@ import AlchemyPayDeposit from "./AlchemyPay";
 
 function AddFunds() {
   return (
-    <div className="my-4 flex flex-col gap-4">
-      <div className="mb-4 pl-[18px] text-lg">
-        <strong>Add funds to your wallet</strong>
-      </div>
-      <AlchemyPayDeposit />
-    </div>
+    // <div className="my-4 flex flex-col gap-4">
+    //   <div className="mb-4 pl-[18px] text-lg">
+    //     <strong>Add funds to your wallet</strong>
+    //   </div>
+    //   <AlchemyPayDeposit />
+    // </div>
+    <></>
   );
 }
 
