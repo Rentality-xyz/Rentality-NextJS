1647f1c56d9cea7a204a5e43df3ccbda079d7345
Dima Denisenko <dendima80@gmail.com>
2025-11-26 13:32:24 +0200

    bugfix 1165

--- src/features/referralProgram/components/ClaimMyPointsHeaderButton.tsx
+++ src/features/referralProgram/components/ClaimMyPointsHeaderButton.tsx
@@ -13,35 +13,14 @@ export default function ClaimMyPointsHeaderButton() {
   const readyToClaim = useOwnReferralPointsSharedStore((state) => state.readyToClaim);
 
   return (
-    // <button
-    //   className="ml-[116px] hidden items-center rounded-md border border-gray-500 px-4 py-2 hover:border-gray-400 xl:flex"
-    //   disabled={isLoading || isFetching || isClaiming || readyToClaim === 0}
-    //   onClick={() => claimMyPoints()}
-    // >
-    //   <Image src={"/images/icons/ic_star_points_yellow.svg"} width={47} height={47} alt="" className="mr-2 h-7 w-7" />
-    //   <div className="ml-0.5 flex">
-    //     {isClaiming ? (
-    //       <>{t("referrals_and_point.claiming")}</>
-    //     ) : isLoading || isFetching ? (
-    //       <>{t("referrals_and_point.loading")}</>
-    //     ) : (
-    //       <>
-    //         {t("referrals_and_point.claim")}{" "}
-    //         <span className="px-1 font-semibold text-rentality-secondary">{readyToClaim.toString()}</span>{" "}
-    //         {t("referrals_and_point.points")}
-    //       </>
-    //     )}
-    //   </div>
-    // </button>
     <button
-      className="ml-[116px] hidden max-w-[260px] items-center rounded-md border border-gray-500 px-4 py-2 hover:border-gray-400 xl:flex"
+      className="group relative ml-[116px] hidden items-center rounded-md border border-gray-500 px-4 py-2 hover:border-gray-400 xl:flex"
       disabled={isLoading || isFetching || isClaiming || readyToClaim === 0}
       onClick={() => claimMyPoints()}
-      title={`${t("referrals_and_point.claim")} ${readyToClaim.toString()} ${t("referrals_and_point.points")}`}
     >
       <Image src="/images/icons/ic_star_points_yellow.svg" width={47} height={47} alt="" className="mr-2 h-7 w-7" />
 
-      <div className="ml-0.5 min-w-0 flex-1 truncate">
+      <div className="ml-0.5 min-w-0 flex-1 truncate group-hover:flex group-hover:overflow-visible group-hover:whitespace-normal">
         {isClaiming ? (
           t("referrals_and_point.claiming")
         ) : isLoading || isFetching ? (
@@ -49,7 +28,7 @@ export default function ClaimMyPointsHeaderButton() {
         ) : (
           <>
             {t("referrals_and_point.claim")}{" "}
-            <span className="px-1 font-semibold text-rentality-secondary">{readyToClaim.toString()}</span>{" "}
+            <span className="px-1 font-semibold text-rentality-secondary">11777000</span>{" "}
             {t("referrals_and_point.points")}
           </>
         )}
