29708ca71fa0ac4102f3ae7a96fb18c3f1b7c971
Dima Denisenko <dendima80@gmail.com>
2025-11-26 12:46:13 +0200

    bugfix 1165

--- src/features/referralProgram/components/ClaimMyPointsHeaderButton.tsx
+++ src/features/referralProgram/components/ClaimMyPointsHeaderButton.tsx
@@ -13,17 +13,39 @@ export default function ClaimMyPointsHeaderButton() {
   const readyToClaim = useOwnReferralPointsSharedStore((state) => state.readyToClaim);
 
   return (
+    // <button
+    //   className="ml-[116px] hidden items-center rounded-md border border-gray-500 px-4 py-2 hover:border-gray-400 xl:flex"
+    //   disabled={isLoading || isFetching || isClaiming || readyToClaim === 0}
+    //   onClick={() => claimMyPoints()}
+    // >
+    //   <Image src={"/images/icons/ic_star_points_yellow.svg"} width={47} height={47} alt="" className="mr-2 h-7 w-7" />
+    //   <div className="ml-0.5 flex">
+    //     {isClaiming ? (
+    //       <>{t("referrals_and_point.claiming")}</>
+    //     ) : isLoading || isFetching ? (
+    //       <>{t("referrals_and_point.loading")}</>
+    //     ) : (
+    //       <>
+    //         {t("referrals_and_point.claim")}{" "}
+    //         <span className="px-1 font-semibold text-rentality-secondary">{readyToClaim.toString()}</span>{" "}
+    //         {t("referrals_and_point.points")}
+    //       </>
+    //     )}
+    //   </div>
+    // </button>
     <button
-      className="ml-[116px] hidden items-center rounded-md border border-gray-500 px-4 py-2 hover:border-gray-400 xl:flex"
+      className="ml-[116px] hidden max-w-[260px] items-center rounded-md border border-gray-500 px-4 py-2 hover:border-gray-400 xl:flex"
       disabled={isLoading || isFetching || isClaiming || readyToClaim === 0}
       onClick={() => claimMyPoints()}
+      title={`${t("referrals_and_point.claim")} ${readyToClaim.toString()} ${t("referrals_and_point.points")}`}
     >
-      <Image src={"/images/icons/ic_star_points_yellow.svg"} width={47} height={47} alt="" className="mr-2 h-7 w-7" />
-      <div className="ml-0.5 flex">
+      <Image src="/images/icons/ic_star_points_yellow.svg" width={47} height={47} alt="" className="mr-2 h-7 w-7" />
+
+      <div className="ml-0.5 min-w-0 flex-1 truncate">
         {isClaiming ? (
-          <>{t("referrals_and_point.claiming")}</>
+          t("referrals_and_point.claiming")
         ) : isLoading || isFetching ? (
-          <>{t("referrals_and_point.loading")}</>
+          t("referrals_and_point.loading")
         ) : (
           <>
             {t("referrals_and_point.claim")}{" "}
