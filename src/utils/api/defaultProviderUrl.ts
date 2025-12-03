af83b8c50a100d136020128b12ee0ef4c98eb636
danyloday <danil.vladyka1@gmail.com>
2025-12-03 02:37:51 +0200

    RTY-1166: MultiCall wrapper

--- src/utils/api/defaultProviderUrl.ts
+++ src/utils/api/defaultProviderUrl.ts
@@ -2,9 +2,10 @@ import { JsonRpcProvider } from "ethers";
 import { env } from "../env";
 import getProviderApiUrlFromEnv from "./providerApiUrl";
 import { fetchDefaultRpcUrl } from "@/features/notifications/utils/fetchDefaultRpcUrl";
+import { MulticallWrapper } from "ethers-multicall-provider";
 
 
 export default async function getDefaultProvider() {
-    const url = await fetchDefaultRpcUrl(env.NEXT_PUBLIC_DEFAULT_CHAIN_ID)
-    return new JsonRpcProvider(url)
+    const url = await fetchDefaultRpcUrl(env.NEXT_PUBLIC_DEFAULT_CHAIN_ID);
+    return MulticallWrapper.wrap(new JsonRpcProvider(url));
 }
