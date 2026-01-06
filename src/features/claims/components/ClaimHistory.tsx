ad089dd7437c8b752a5a54b54c22e7528180b273
Gleb_Protasov <hlibaltrecipe@gmail.com>
2026-01-06 16:27:22 +0200

    rty 1162

--- src/features/claims/components/ClaimHistory.tsx
+++ src/features/claims/components/ClaimHistory.tsx
@@ -35,7 +35,6 @@ const headerSpanClassName = "text-start px-2 font-light text-sm";
 const rowSpanClassName = "px-2 h-16";
 
 export default function ClaimHistory({ claims }: Props) {
-  console.log(claims.find(cancelClaim => cancelClaim.claimId === 120));
   const ethereumInfo = useEthereum();
   const { userMode, isHost } = useUserMode();
   const { mutateAsync: payClaim } = usePayClaim();
